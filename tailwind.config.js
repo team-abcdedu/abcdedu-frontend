e08dce14376dc89d1f6f1babb9646e606ddcafee
mihee <s2809h29@gmail.com>
2024-08-20 19:17:51 +0900

    feat: tailwind 초기 세팅 #1

---
+++ tailwind.config.js
@@ -0,0 +1,21 @@
+/** @type {import('tailwindcss').Config} */
+
+const px0_10 = { ...Array.from(Array(11)).map((_, i) => `${i}px`) };
+const px0_100 = { ...Array.from(Array(101)).map((_, i) => `${i}px`) };
+const px0_300 = { ...Array.from(Array(301)).map((_, i) => `${i}px`) };
+
+module.exports = {
+  content: ['./src/**/*.{js,jsx,ts,tsx}'],
+  theme: {
+    extend: {
+      // TODO: color, font ...
+      borderWidth: px0_10,
+      fontSize: px0_100,
+      lineHeight: px0_100,
+      minWidth: px0_300,
+      minHeight: px0_300,
+      spacing: px0_300,
+    },
+  },
+  plugins: [],
+};
