192c0e71bf4356e40cd252ba7c71552dcae5fdac
mihee <s2809h29@gmail.com>
2024-08-20 21:05:03 +0900

    feat: React-Query 세팅 #1

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,20 @@
+import { QueryClientProvider } from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import { RouterProvider } from 'react-router-dom';
 
 import './App.css';
+import { queryClient } from './libs/react-query';
 import router from './routes';
 
 function App() {
-  return <RouterProvider router={router} />;
+  return (
+    <>
+      <QueryClientProvider client={queryClient}>
+        <ReactQueryDevtools />
+        <RouterProvider router={router} />
+      </QueryClientProvider>
+    </>
+  );
 }
 
 export default App;
