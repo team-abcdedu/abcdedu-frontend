6b5efddc8387c9164293e47492da7f9f66139787
mihee <s2809h29@gmail.com>
2024-08-21 00:09:07 +0900

    feat: lint 규칙 수정 #1

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -2,6 +2,7 @@ module.exports = {
   root: true,
   env: { browser: true, es2020: true, node: true },
   extends: [
+    'airbnb-base',
     'eslint:recommended',
     'plugin:import/recommended',
     'plugin:import/typescript',
@@ -31,6 +32,16 @@ module.exports = {
     'jsx-a11y/anchor-is-valid': 'off',
     'jsx-a11y/click-events-have-key-events': 'off',
     'import/no-named-as-default': 'off',
+    'import/extensions': [
+      'error',
+      'ignorePackages',
+      {
+        js: 'never',
+        jsx: 'never',
+        ts: 'never',
+        tsx: 'never',
+      },
+    ],
     'import/order': [
       'error',
       {
@@ -47,6 +58,11 @@ module.exports = {
         alphabetize: { order: 'asc', caseInsensitive: true },
       },
     ],
+    'import/prefer-default-export': 0,
+    'no-alert': 'off',
+    'no-console': 'off',
+    camelcase: 'off',
+    'import/no-extraneous-dependencies': ['error', { devDependencies: true }],
     'prettier/prettier': ['error', { endOfLine: 'auto' }],
   },
 };
