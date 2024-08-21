40db584f3f69f7e6bf5bc82dd9f7b64100c4d2f6
PHJoon <phj9503@gmail.com>
2024-08-22 08:46:06 +0900

    refactor: 헤더 메뉴 분리

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,15 +1,9 @@
-import { List } from '@phosphor-icons/react';
-import { useState } from 'react';
 import { Link } from 'react-router-dom';
 
 import HeaderMenu from '@/components/Header/HeaderMenu';
-import HeaderNav from '@/components/Header/HeaderNav';
+import MobileMenu from '@/components/Header/MobileMenu';
 
 function Header() {
-  const [isMenuOpen, setIsMenuOpen] = useState(false);
-  const headerBtnStyle =
-    'px-24 min-h-40 max-h-46 grid place-items-center rounded-md';
-
   return (
     <header
       className={
@@ -19,37 +13,9 @@ function Header() {
       <div className={'grid place-items-center'}>
         <Link to={'/'}>ABCDEdu</Link>
       </div>
-      <div className={'grid place-items-center'}>
-        <div className={'hidden md:flex flex-end gap-12'}>
-          <HeaderNav />
-          <button
-            className={`${headerBtnStyle} border-1 border-blue-800 text-blue-800 hover:bg-slate-200`}
-          >
-            로그인
-          </button>
-          <button
-            className={`${headerBtnStyle} bg-blue-800 text-white hover:opacity-80`}
-          >
-            회원가입
-          </button>
-          <select
-            className={
-              'p-6 grid place-items-center w-80 font-extralight underline text-14'
-            }
-            defaultValue={'Korean'}
-          >
-            <option>Korean</option>
-            <option>English</option>
-          </select>
-        </div>
-        <div className={'md:hidden flex flex-col flex-end '}>
-          <div>
-            <div className={`grid place-items-center`}>
-              <List size={24} onClick={() => setIsMenuOpen(!isMenuOpen)} />
-            </div>
-            {isMenuOpen && <HeaderMenu />}
-          </div>
-        </div>
+      <div>
+        <HeaderMenu />
+        <MobileMenu />
       </div>
     </header>
   );
--- src/components/header/HeaderMenu.tsx
+++ src/components/header/HeaderMenu.tsx
@@ -1,38 +1,11 @@
-import { Link } from 'react-router-dom';
-
-import { headerMenuNavItems } from '@/constants/navItems';
-import { HeaderMenuNavItem } from '@/types/navTypes';
+import HeaderButtons from '@/components/Header/HeaderButtons';
+import HeaderNav from '@/components/Header/HeaderNav';
 
 function HeaderMenu() {
-  const menuButtonStyle = 'min-w-218 min-h-40 w-auto text-15 px-12 rounded';
-
   return (
-    <div className={'absolute bg-white right-16 rounded-md'}>
-      <li className={`flex flex-col gap-8 px-16 py-8 my-12 bg-white`}>
-        {headerMenuNavItems.map((item: HeaderMenuNavItem) => {
-          return (
-            <Link
-              key={item.to}
-              to={item.to}
-              className={
-                'min-w-218 min-h-32 grid place-items-center text-15 px-12 hover:bg-slate-200'
-              }
-            >
-              {item.text}
-            </Link>
-          );
-        })}
-        <button
-          className={`${menuButtonStyle} border-1 border-blue-800 text-blue-800 hover:bg-slate-200`}
-        >
-          로그인
-        </button>
-        <button
-          className={`${menuButtonStyle} bg-blue-800 text-white hover:opacity-80`}
-        >
-          회원가입
-        </button>
-      </li>
+    <div className={'hidden md:flex flex-end gap-12'}>
+      <HeaderNav />
+      <HeaderButtons mobile={false} />
     </div>
   );
 }
