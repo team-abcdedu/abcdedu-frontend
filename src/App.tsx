00dd221bf5c9ca3319a9defb9898b6520b238970
mihee <s2809h29@gmail.com>
2024-08-21 17:55:28 +0900

    rename: global style 파일명 수정 및 위치 이동

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import { QueryClientProvider } from '@tanstack/react-query';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import { RouterProvider } from 'react-router-dom';
 
-import './App.css';
+import '@/styles/global.css';
 import { queryClient } from './libs/react-query';
 import router from './routes';
 
