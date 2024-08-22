7cbee2bb2cc6acd419335f85a9c3bf29a4eb532a
yjenis <icdiyj147@gmail.com>
2024-08-22 13:36:18 +0900

    feat: footer 짧은 버전 구현

---
+++ src/components/Footer/FooterShort.tsx
@@ -0,0 +1,13 @@
+import FooterNav from './FooterNav';
+import FooterSns from './FooterSns';
+
+function Footer() {
+  return (
+    <div className='text-center text-xs text-gray-600'>
+      <FooterSns />
+      <FooterNav />
+    </div>
+  );
+}
+
+export default Footer;
