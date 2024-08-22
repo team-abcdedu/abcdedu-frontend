10eda94fca3b03f43280711061e5647b862e47ec
yjenis <icdiyj147@gmail.com>
2024-08-22 12:47:41 +0900

    feat: sns 이동 기능 구현

--- src/components/Footer/FooterSns.tsx
+++ src/components/Footer/FooterSns.tsx
@@ -1,29 +1,42 @@
-import { FaInstagram } from 'react-icons/fa';
+import { FaInstagram, FaYoutube } from 'react-icons/fa';
+import { RiKakaoTalkFill } from 'react-icons/ri';
 import { Link } from 'react-router-dom';
+
+import naverBand from '../../assets/naver band.png';
+import naverBlog from '../../assets/naver blog.png';
 
 function FooterSns() {
   return (
-    <div className='flex justify-center space-x-4 mb-4'>
-      <Link to='#' className='text-black hover:text-gray-500'>
-        <FaInstagram />
-        {/* <img src="<FaInstagram />" alt="Instagram" className="h-8 w-8" /> */}
+    <div className='flex justify-center space-x-20 my-8'>
+      <Link
+        to='https://www.instagram.com/abcd_education/'
+        className='text-black'
+      >
+        <FaInstagram className='h-20 w-20' />
       </Link>
-      <Link to='#' className='text-black hover:text-gray-500'>
+
+      <Link to='https://www.youtube.com/@abcd_edu' className='text-black'>
+        <FaYoutube className='h-20 w-20' />
+      </Link>
+
+      <Link to='https://band.us/@abcdedu' className='text-black'>
         <img
-          src='/path-to-your-youtube-icon.png'
-          alt='YouTube'
-          className='h-8 w-8'
+          src={naverBand}
+          alt='Naver Band'
+          className='h-20 w-20 object-contain'
         />
       </Link>
-      <Link to='#' className='text-black hover:text-gray-500'>
-        <img src='/path-to-your-blog-icon.png' alt='Blog' className='h-8 w-8' />
-      </Link>
-      <Link to='#' className='text-black hover:text-gray-500'>
+
+      <Link to='https://blog.naver.com/abcdedu' className='text-black'>
         <img
-          src='/path-to-your-talk-icon.png'
-          alt='KakaoTalk'
-          className='h-8 w-8'
+          src={naverBlog}
+          alt='Naver Band'
+          className='h-20 w-20 object-contain'
         />
+      </Link>
+
+      <Link to='https://blog.naver.com/abcdedu' className='text-black'>
+        <RiKakaoTalkFill className='h-20 w-20' />
       </Link>
     </div>
   );
--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,14 +1,14 @@
 import { Outlet } from 'react-router-dom';
 
+import Footer from '@/components/Footer/Footer';
 import Header from '@/components/header/Header';
-// import Footer from '@/components/Footer/Footer';
 
 function Layout() {
   return (
     <>
       <Header />
       <Outlet />
-      {/* <Footer/> */}
+      <Footer />
     </>
   );
 }
