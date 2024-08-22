a5decfbc9197cb557fce41a66890a085ab0a02f3
PHJoon <phj9503@gmail.com>
2024-08-22 12:23:35 +0900

    fix: 드롭다운 간격 수정

--- src/components/Dropdown.tsx
+++ src/components/Dropdown.tsx
@@ -25,7 +25,7 @@ function Dropdown(props: PropsType) {
         {isHovering && (
           <div className={'overflow-hidden absolute'}>
             <li
-              className={`flex flex-col gap-6 bg-white py-6 ${isHovering ? 'animate-dropdown' : ''} ${contentsContainerStyle}`}
+              className={`flex flex-col gap-7 bg-white py-7 ${isHovering ? 'animate-dropdown' : ''} ${contentsContainerStyle}`}
             >
               {children}
             </li>
