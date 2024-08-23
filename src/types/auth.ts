aa8347a690e003c581e3365c72ed6bffdbe414bd
mihee <s2809h29@gmail.com>
2024-08-23 17:29:38 +0900

    feat: AuthModal에 포함되는 타입 추가 #8

---
+++ src/types/auth.ts
@@ -0,0 +1,10 @@
+export type AuthModalType = 'login' | 'register';
+
+export interface AuthModalProps {
+  type?: AuthModalType;
+  isVisible: boolean;
+  onClose: () => void;
+  onToggle: () => void;
+}
+
+export type AuthModalActions = Pick<AuthModalProps, 'onClose' | 'onToggle'>;
