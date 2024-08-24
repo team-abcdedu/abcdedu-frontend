439450486c6d19a565c91ba70dd4f5809947cfa3
PHJoon <phj9503@gmail.com>
2024-08-24 09:27:33 +0900

    feat: 커리큘럼 imgUrl 추가

---
+++ src/pages/Classes/constants/imgUrl.ts
@@ -0,0 +1,2 @@
+export const curriculumImgUrl =
+  'https://c48aa73694122edae77e6fe97fafbfbe.cdn.bubble.io/f1723138717697x280065903378054820/%E1%84%8F%E1%85%A5%E1%84%85%E1%85%B5%E1%84%8F%E1%85%B2%E1%86%AF%E1%84%85%E1%85%A5%E1%86%B7.svg';
