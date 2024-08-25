6cbc7a863b67e9ed053f9a1d90ed76f4d0209d25
yjenis <icdiyj147@gmail.com>
2024-08-25 23:11:59 +0900

    feat: communityInfo 데이터 constants에 삽입

---
+++ src/pages/Community/constants/communityInfo.ts
@@ -0,0 +1,46 @@
+import { CommunityInfoMap } from '@/types/communityTypes';
+
+export const classInfoMap: CommunityInfoMap = {
+  abcdProject: {
+    label: 'ABCD Project',
+    description:
+      '세상의 혁신이 시작되는 곳, 여러분의 ABCD 프로젝트를 공유해주세요!',
+    communities: [
+      {
+        code: 'ABCD Project',
+        title:
+          '세상의 혁신이 시작되는 곳, 여러분의 ABCD 프로젝트를 공유해주세요!',
+      },
+    ],
+  },
+  qna: {
+    label: 'Q & A',
+    description: '무엇이든 물어보세요!',
+    communities: [
+      {
+        code: 'Q & A',
+        title: '무엇이든 물어보세요!',
+      },
+    ],
+  },
+  submitAssignment: {
+    label: '과제 제출',
+    description: '여러분의 프로젝트를 자랑해 주세요!',
+    communities: [
+      {
+        code: '과제 제출',
+        title: '여러분의 프로젝트를 자랑해 주세요!',
+      },
+    ],
+  },
+  bulletinBoard: {
+    label: '자유게시판',
+    description: '고교학점제 관련 정보를 공유해보아요!',
+    communities: [
+      {
+        code: '자유게시판',
+        title: '고교학점제 관련 정보를 공유해보아요!',
+      },
+    ],
+  },
+};
