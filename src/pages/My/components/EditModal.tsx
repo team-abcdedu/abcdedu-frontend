ca8666fb436b22ea98564c6a68379673dda01f50
mihee <s2809h29@gmail.com>
2024-08-27 23:39:37 +0900

    feat: 프로필 편집 모달 퍼블리싱 #24

---
+++ src/pages/My/components/EditModal.tsx
@@ -0,0 +1,25 @@
+import Modal from '@/components/Modal';
+
+import { ProfileEditModalProps } from '../types';
+
+import EditAccount from './EditAccount';
+import EditProfile from './EditProfile';
+
+export default function EditModal({
+  type = 'profile',
+  isVisible,
+  onClose,
+  onToggle,
+}: ProfileEditModalProps) {
+  const IS_PROFILE_MODAL = type === 'profile';
+
+  return (
+    <Modal isVisible={isVisible} onClose={onClose}>
+      {IS_PROFILE_MODAL ? (
+        <EditProfile onClose={onClose} onToggle={onToggle} />
+      ) : (
+        <EditAccount onToggle={onToggle} />
+      )}
+    </Modal>
+  );
+}
