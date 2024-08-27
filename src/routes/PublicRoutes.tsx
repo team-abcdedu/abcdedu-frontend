2e7ce1f830ad60fdb479ac09d971a535a2951d24
PHJoon <phj9503@gmail.com>
2024-08-27 12:08:17 +0900

    fix: router index 수정

--- src/routes/PublicRoutes.tsx
+++ src/routes/PublicRoutes.tsx
@@ -1,5 +1,6 @@
 import { RouteObject } from 'react-router-dom';
 
+import Layout from '@/components/Layout';
 import Classes from '@/pages/Classes';
 import Course from '@/pages/Classes/[courseId]';
 import Class from '@/pages/Classes/[courseId]/[classCode]';
@@ -8,22 +9,24 @@ import Home from '@/pages/Home';
 export const publicRoutes: RouteObject[] = [
   {
     path: '/',
-    index: true,
-    element: <Home />,
-  },
-  {
-    path: '/classes',
-    index: true,
-    element: <Classes />,
-  },
-  {
-    path: '/classes/:courseId',
-    index: true,
-    element: <Course />,
-  },
-  {
-    path: '/classes/:courseId/:classCode',
-    index: true,
-    element: <Class />,
+    element: <Layout />, // 부모 컴포넌트
+    children: [
+      {
+        index: true, // 기본적으로 렌더링되는 컴포넌트
+        element: <Home />,
+      },
+      {
+        path: '/classes',
+        element: <Classes />,
+      },
+      {
+        path: '/classes/:courseId',
+        element: <Course />,
+      },
+      {
+        path: '/classes/:courseId/:classCode',
+        element: <Class />,
+      },
+    ],
   },
 ];
