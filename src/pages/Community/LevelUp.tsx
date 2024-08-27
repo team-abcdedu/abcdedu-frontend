dd73c27be60861e5d98858238845b769ce8ddf3e
yjenis <icdiyj147@gmail.com>
2024-08-27 11:31:03 +0900

    feat: 상단 게시판 정보 구현

--- src/pages/Community/LevelUp.tsx
+++ src/pages/Community/LevelUp.tsx
@@ -1,5 +1,12 @@
 function LevelUp() {
-  return <div>LevelUp</div>;
+  return (
+    <div className='flex flex-col text-center mt-20'>
+      <div className='py-30'>
+        <p className='text-gray-400'>ABCDEdu 커뮤니티</p>
+        <h3 className='text-primary-400 text-30 font-bold'>등업 게시판</h3>
+      </div>
+    </div>
+  );
 }
 
 export default LevelUp;
