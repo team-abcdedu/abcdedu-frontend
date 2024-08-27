d9efcaf74fbb7904a20244019c4dceaea69394e6
PHJoon <phj9503@gmail.com>
2024-08-27 16:37:56 +0900

    feat: Auth 모달 다시 추가

--- src/components/Header/HeaderButtons.tsx
+++ src/components/Header/HeaderButtons.tsx
@@ -1,13 +1,30 @@
+import { useState } from 'react';
+
 import UserLogin from '@/assets/icons/user-login.svg?react';
+import AuthModal from '@/components/Header/AuthModal';
+import useModal from '@/hooks/useModal';
+import { AuthModalType } from '@/types/auth';
 
 function HeaderButtons() {
   const btnStyle =
     'w-100 h-36 px-20 py-4 flex-row-center rounded-[20px] text-14';
+  const { isVisible, toggleModal } = useModal();
+  const [modalType, setModalType] = useState<AuthModalType>('login');
+
+  const handleClick = (type: AuthModalType) => {
+    toggleModal();
+    setModalType(type);
+  };
+
+  const toggleModalType = () => {
+    setModalType(modalType === 'login' ? 'register' : 'login');
+  };
 
   return (
     <div className={'flex flex-col items-center sm:flex-row gap-10'}>
       <button
         className={'hidden sm:flex w-100 h-36 text-14 items-center gap-4'}
+        onClick={() => handleClick('login')}
       >
         <UserLogin className={'w-30 h-30'} />
         <div
@@ -18,12 +35,24 @@ function HeaderButtons() {
           로그인
         </div>
       </button>
-      <button className={`block sm:hidden ${btnStyle} btn-white-pb`}>
+      <button
+        className={`block sm:hidden ${btnStyle} btn-white-pb`}
+        onClick={() => handleClick('login')}
+      >
         로그인
       </button>
-      <button className={`${btnStyle} bg-primary-400 text-white`}>
+      <button
+        className={`${btnStyle} bg-primary-400 text-white`}
+        onClick={() => handleClick('register')}
+      >
         회원가입
       </button>
+      <AuthModal
+        type={modalType}
+        isVisible={isVisible}
+        onClose={toggleModal}
+        onToggle={toggleModalType}
+      />
     </div>
   );
 }
