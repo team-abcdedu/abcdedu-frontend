c047da41bbfa0c1871bcaeb4a7ad096973776f88
mihee <s2809h29@gmail.com>
2024-08-31 00:05:25 +0900

    feat: 로그인 form 유효성 검사 추가 및 form 태그 위치 변경 #50
    - 백엔드에 유효성 검사 및 관련 예외 응답이 더 존재하여 프론트도 그에 맞춰 유효성 검사 추가
    - enter 키 이벤트로도 form 제출 가능하도록 제출 버튼 위치 변경

--- src/components/Header/AuthModal/Login.tsx
+++ src/components/Header/AuthModal/Login.tsx
@@ -7,10 +7,12 @@ import { AuthModalActions } from '@/types/auth';
 export default function Login({ onClose, onToggle }: AuthModalActions) {
   const fieldStyle = 'flex flex-col gap-4 [&>label]:text-14';
 
-  const { hasErrors, getErrorMessage, register, onSubmit } = useLoginForm();
+  const { fieldRules, errors, register, onSubmit } = useLoginForm({
+    onSuccess: onClose,
+  });
 
   return (
-    <>
+    <form onSubmit={onSubmit}>
       <Modal.Content>
         <div>
           <button
@@ -21,34 +23,39 @@ export default function Login({ onClose, onToggle }: AuthModalActions) {
             <X size={24} />
           </button>
           <h2 className='text-24 font-semibold mb-24'>로그인</h2>
-          <form className='flex flex-col gap-12 mb-4'>
+          <div className='flex flex-col gap-12 mb-4'>
             <div className={fieldStyle}>
               <label htmlFor='email'>이메일</label>
               <input
-                {...register('email', { required: true })}
+                {...register('email', fieldRules.email)}
                 id='email'
                 type='text'
                 className='input-primary'
                 placeholder='johndoe@gmail.com'
               />
+              {errors.email && (
+                <span className='text-12 text-red-500'>
+                  {errors.email.message}
+                </span>
+              )}
             </div>
             <div className={fieldStyle}>
               <label htmlFor='password'>비밀번호</label>
               <input
-                {...register('password', { required: true })}
+                {...register('password', fieldRules.password)}
                 id='password'
                 type='password'
                 className='input-primary'
                 placeholder='********'
                 autoComplete='false'
               />
+              {errors.password && (
+                <span className='text-12 text-red-500'>
+                  {errors.password.message}
+                </span>
+              )}
             </div>
-            {hasErrors && (
-              <span className='text-12 text-center text-red-500'>
-                {getErrorMessage()}를 입력하세요.
-              </span>
-            )}
-          </form>
+          </div>
           {/* 고도화 단계에서 개발 */}
           {/* <Link className='flex justify-end text-14 text-primary-300' to='/'>
             비밀번호 찾기
@@ -60,7 +67,6 @@ export default function Login({ onClose, onToggle }: AuthModalActions) {
           type='submit'
           className='w-full h-45 px-24 bg-primary-300 text-15 
         text-white font-semibold rounded-md'
-          onClick={onSubmit}
         >
           로그인
         </button>
@@ -72,6 +78,6 @@ export default function Login({ onClose, onToggle }: AuthModalActions) {
           계정 생성하기
         </button>
       </Modal.Actions>
-    </>
+    </form>
   );
 }
--- src/hooks/auth/useLoginForm.ts
+++ src/hooks/auth/useLoginForm.ts
@@ -1,38 +1,55 @@
 import { useForm, FieldValues } from 'react-hook-form';
 
+import { FieldRules } from '@/types';
+import { UseAuthFormProps } from '@/types/auth';
+
 interface ILoginFormInput {
   email: string;
   password: string;
 }
 
-export default function useLoginForm() {
+export default function useLoginForm({ onSuccess }: UseAuthFormProps) {
   const {
     formState: { errors },
     register,
     handleSubmit,
-  } = useForm<ILoginFormInput>({ reValidateMode: 'onSubmit' }); // 폼 제출 시에만 재검증
+  } = useForm<ILoginFormInput>();
 
-  const hasErrors = Object.keys(errors).length > 0;
-
-  const getErrorMessage = () => {
-    const emptyFields = [];
-    if (errors.email) emptyFields.push('이메일');
-    if (errors.password) emptyFields.push('비밀번호');
-    return emptyFields.join(', ');
+  const fieldRules: FieldRules<ILoginFormInput> = {
+    email: {
+      required: '이메일을 입력하세요.',
+      pattern: {
+        value: /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/,
+        message: '올바르지 않은 이메일 형식입니다.',
+      },
+    },
+    password: {
+      required: '비밀번호를 입력하세요.',
+      minLength: {
+        value: 6,
+        message: '비밀번호는 최소 6자 이상이어야 합니다.',
+      },
+      maxLength: {
+        value: 20,
+        message: '비밀번호는 최대 20자까지 입력할 수 있습니다.',
+      },
+    },
   };
 
-  const login = (data: FieldValues) => {
-    alert(JSON.stringify(data)); // 확인용, 추후 제거
-    // login
+  const login = async (data: FieldValues) => {
+    const { email, password } = data;
+    console.log(email, password);
+    onSuccess();
   };
 
-  const onSubmit = () => {
+  const onSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
     handleSubmit(login)();
   };
 
   return {
-    hasErrors,
-    getErrorMessage,
+    fieldRules,
+    errors,
     register,
     onSubmit,
   };
