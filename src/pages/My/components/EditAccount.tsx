991c05cde5daaf7547db470fc7de07dbd06ca3d6
mihee <s2809h29@gmail.com>
2024-08-31 14:21:39 +0900

    feat: 계정 정보 수정 유효성 검사 #53

--- src/pages/My/components/EditAccount.tsx
+++ src/pages/My/components/EditAccount.tsx
@@ -2,6 +2,7 @@ import { X } from '@phosphor-icons/react';
 
 import Modal from '@/components/Modal';
 
+import useAccountForm from '../hooks/useAccountForm';
 import { ProfileEditProps } from '../types';
 
 export default function EditAccount({ onClose, onToggle }: ProfileEditProps) {
@@ -9,43 +10,85 @@ export default function EditAccount({ onClose, onToggle }: ProfileEditProps) {
   const inputStyle =
     'px-0 py-4 rounded-none border-b-1 border-zinc-300 border-neutral-500 focus:border-primary-300 text-neutral-600 transition-2';
 
-  const handleClose = () => {
-    onClose();
-    onToggle(); // 프로필 수정 모달로 초기화
-  };
+  const { errors, fieldRules, register, onSubmit } = useAccountForm({
+    onSuccess: onClose,
+  });
 
   return (
-    <>
+    <form onSubmit={onSubmit}>
       <Modal.Content>
         <div className='px-16'>
           <button
             type='button'
             className='absolute top-12 right-12'
-            onClick={handleClose}
+            onClick={onClose}
           >
             <X size={24} />
           </button>
           <h2 className='text-20 text-center font-semibold pt-12 mb-24'>
             회원 정보 수정
           </h2>
-          <form className='flex flex-col gap-20 mb-4 [&_span]:text-14'>
+          <div className='flex flex-col gap-20 mb-4 [&_span]:text-14'>
             <div className={fieldStyle}>
               <span>이메일</span>
-              <input id='name' type='text' className={inputStyle} />
+              <input
+                {...register('email', fieldRules.email)}
+                id='email'
+                type='text'
+                className={inputStyle}
+              />
+              {errors.email && (
+                <span className='!text-12 mt-2 text-red-500'>
+                  {errors.email.message}
+                </span>
+              )}
             </div>
             <div className={fieldStyle}>
               <span>현재 비밀번호</span>
-              <input id='currentPw' type='password' className={inputStyle} />
+              <input
+                {...register('password', fieldRules.password)}
+                id='currentPw'
+                type='password'
+                autoComplete='false'
+                className={inputStyle}
+              />
+              {errors.password && (
+                <span className='!text-12 mt-2 text-red-500'>
+                  {errors.password.message}
+                </span>
+              )}
             </div>
             <div className={fieldStyle}>
               <span>새 비밀번호</span>
-              <input id='newPw' type='password' className={inputStyle} />
+              <input
+                {...register('newPw', fieldRules.newPw)}
+                id='newPw'
+                type='password'
+                autoComplete='false'
+                className={inputStyle}
+              />
+              {errors.newPw && (
+                <span className='!text-12 mt-2 text-red-500'>
+                  {errors.newPw.message}
+                </span>
+              )}
             </div>
             <div className={fieldStyle}>
               <span>새 비밀번호 확인</span>
-              <input id='confirmPw' type='password' className={inputStyle} />
+              <input
+                {...register('confirmPw', fieldRules.confirmPw)}
+                id='confirmPw'
+                type='password'
+                autoComplete='false'
+                className={inputStyle}
+              />
+              {errors.confirmPw && (
+                <span className='!text-12 mt-2 text-red-500'>
+                  {errors.confirmPw.message}
+                </span>
+              )}
             </div>
-          </form>
+          </div>
           <button
             type='button'
             className='block mx-auto mt-20 text-14 font-bold underline text-neutral-500 underline-offset-2'
@@ -57,10 +100,10 @@ export default function EditAccount({ onClose, onToggle }: ProfileEditProps) {
       </Modal.Content>
       <Modal.Actions direction='row'>
         <button
-          type='submit'
+          type='button'
           className='w-full h-45 px-24 text-15 
         text-primary-400 btn-white-pb font-semibold rounded-md'
-          onClick={handleClose}
+          onClick={onClose}
         >
           취소
         </button>
@@ -72,6 +115,6 @@ export default function EditAccount({ onClose, onToggle }: ProfileEditProps) {
           변경사항 저장
         </button>
       </Modal.Actions>
-    </>
+    </form>
   );
 }
