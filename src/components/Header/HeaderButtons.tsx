17f77fd5bb213d9a466ea6eb693bb18fd0bd21c2
mihee <s2809h29@gmail.com>
2024-08-31 12:12:35 +0900

    feat: 로그인 여부에 따라 버튼 렌더링 분기 처리 #50

--- src/components/Header/HeaderButtons.tsx
+++ src/components/Header/HeaderButtons.tsx
@@ -1,13 +1,41 @@
 import { useState } from 'react';
+import { Link } from 'react-router-dom';
 
 import UserLogin from '@/assets/icons/user-login.svg?react';
 import AuthModal from '@/components/Header/AuthModal';
 import useModal from '@/hooks/useModal';
+import useBoundStore from '@/stores';
 import { AuthModalType } from '@/types/auth';
 
-function HeaderButtons() {
-  const btnStyle =
-    'w-100 h-36 px-20 py-4 flex-row-center rounded-[20px] text-14';
+const btnStyle = 'w-100 h-36 px-20 py-4 flex-row-center rounded-[20px] text-14';
+
+function MemberButtons({ name }: { name: string }) {
+  return (
+    <>
+      <Link
+        className={'hidden sm:flex w-100 h-36 text-14 items-center gap-4'}
+        to='/mypage'
+      >
+        <UserLogin className={'w-30 h-30'} />
+        <div
+          className={
+            'w-64 h-36 flex-row-center text-14 font-semibold text-primary-400'
+          }
+        >
+          {name}
+        </div>
+      </Link>
+      <button
+        className={`${btnStyle} bg-primary-400 text-white`}
+        onClick={() => {}}
+      >
+        로그아웃
+      </button>
+    </>
+  );
+}
+
+function GuestButtons() {
   const { isVisible, toggleModal } = useModal();
   const [modalType, setModalType] = useState<AuthModalType>('login');
 
@@ -21,7 +49,7 @@ function HeaderButtons() {
   };
 
   return (
-    <div className={'flex flex-col items-center sm:flex-row gap-10'}>
+    <>
       <button
         className={'hidden sm:flex w-100 h-36 text-14 items-center gap-4'}
         onClick={() => handleClick('login')}
@@ -53,6 +81,16 @@ function HeaderButtons() {
         onClose={toggleModal}
         onToggle={toggleModalType}
       />
+    </>
+  );
+}
+
+function HeaderButtons() {
+  const user = useBoundStore(state => state.user);
+
+  return (
+    <div className={'flex flex-col items-center sm:flex-row gap-10'}>
+      {user ? <MemberButtons name={user.name} /> : <GuestButtons />}
     </div>
   );
 }
