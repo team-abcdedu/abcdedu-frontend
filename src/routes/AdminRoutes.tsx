008e0e5dfa922c2a4c90ba6b10b57a808934952b
PHJoon <phj9503@gmail.com>
2024-09-02 17:09:14 +0900

    feat: admin 경로 추가

--- src/routes/AdminRoutes.tsx
+++ src/routes/AdminRoutes.tsx
@@ -1,3 +1,32 @@
 import { RouteObject } from 'react-router-dom';
 
-export const adminRoutes: RouteObject[] = [];
+import AdminLayout from '@/components/AdminLayout';
+import AdminRoute from '@/components/AdminRoute';
+import Admin from '@/pages/Admin';
+import Assignment from '@/pages/Admin/Assignment';
+import Survey from '@/pages/Admin/Survey';
+
+export const adminRoutes: RouteObject[] = [
+  {
+    path: '/admin',
+    element: (
+      <AdminRoute>
+        <AdminLayout />
+      </AdminRoute>
+    ),
+    children: [
+      {
+        index: true,
+        element: <Admin />,
+      },
+      {
+        path: '/admin/assignment',
+        element: <Assignment />,
+      },
+      {
+        path: '/admin/survey',
+        element: <Survey />,
+      },
+    ],
+  },
+];
