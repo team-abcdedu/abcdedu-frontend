d46f034fe3960012e95204b3d3458dd37fca48ae
mihee <s2809h29@gmail.com>
2024-09-02 21:28:40 +0900

    feat: 프로필 조회, 프로필 업데이트 api 추가 #53

--- src/services/user.ts
+++ src/services/user.ts
@@ -1,9 +1,19 @@
-import { get } from '@/libs/api';
-import { User } from '@/types/user';
+import { get, patch } from '@/libs/api';
+import { User, UserInfo } from '@/types/user';
 
 class UserApi {
   static async getNameAndRole() {
     return get<User>('/members/info/name-and-role');
+  }
+
+  static async getUserInfo() {
+    return get<UserInfo>('/members/info');
+  }
+
+  static async updateUserInfo(form: FormData) {
+    return patch('/members/info', form, {
+      headers: { 'Content-Type': 'multipart/form-data' },
+    });
   }
 }
 
