8b60a5650221b0752fa6145232b48658a882e440
mihee <s2809h29@gmail.com>
2024-09-03 11:12:05 +0900

    fix: ContactItem 타입 수정 #65

--- src/pages/contact/constants/index.ts
+++ src/pages/contact/constants/index.ts
@@ -6,21 +6,21 @@ import { ContactItem } from '../types';
 
 export const contactItems: ContactItem[] = [
   {
-    type: 'lecture', // 임시
+    type: 'class',
     label: '강의 의뢰',
     message:
       'ABCDEdu가 제공하는 교육 프로그램에 관심이 있으시면, 간단한 양식을 작성해 주세요. 빠르게 연락드리겠습니다.',
     icon: Envelope,
   },
   {
-    type: 'teacher training',
+    type: 'training',
     label: '교사 연수 의뢰',
     message:
       'ABCDEdu가 제공하는 교육 프로그램에 관심이 있으시면, 간단한 양식을 작성해 주세요. 빠르게 연락드리겠습니다.',
     icon: EnvelopeO,
   },
   {
-    type: 'inquiry',
+    type: 'etc',
     label: '문의 남기기',
     message: '문의 사항을 남겨주시면 빠르게 연락드리겠습니다.',
     icon: Inbox,
