cb00df51508c2e034bd8faf3ac671ef3f4bd67b7
mihee <s2809h29@gmail.com>
2024-09-03 16:08:23 +0900

    feat: 갤러리 이미지 데이터 id 추가 #68

--- src/pages/AboutUs/constants/index.ts
+++ src/pages/AboutUs/constants/index.ts
@@ -1,101 +1,125 @@
-export const imageInfo = [
+import { v4 as uuidv4 } from 'uuid';
+
+import { GalleryImage } from '../types';
+
+export const imageInfo: GalleryImage[] = [
   {
+    id: uuidv4(),
     school: '선정고',
     date: '2024-06-11',
     url: 'https://d1muf25xaso8hp.cloudfront.net/https%3A%2F%2Fc48aa73694122edae77e6fe97fafbfbe.cdn.bubble.io%2Ff1719160683333x623348348005079700%2F%25E1%2584%2589%25E1%2585%25A5%25E1%2586%25AB%25E1%2584%258C%25E1%2585%25A5%25E1%2586%25BC%25E1%2584%2580%25E1%2585%25A9%25205-27-24.jpg?w=768&h=&auto=compress&dpr=1.25&fit=max',
   },
   {
+    id: uuidv4(),
     school: '선정고',
     date: '2024-06-11',
     url: 'https://d1muf25xaso8hp.cloudfront.net/https%3A%2F%2Fc48aa73694122edae77e6fe97fafbfbe.cdn.bubble.io%2Ff1719160699215x318627834958592900%2F%25E1%2584%2589%25E1%2585%25A5%25E1%2586%25AB%25E1%2584%258C%25E1%2585%25A5%25E1%2586%25BC%25E1%2584%2580%25E1%2585%25A9%25206-11-24.jpg?w=768&h=&auto=compress&dpr=1.25&fit=max',
   },
   {
+    id: uuidv4(),
     school: '보인고',
     date: '2024-05-22',
     url: 'https://d1muf25xaso8hp.cloudfront.net/https%3A%2F%2Fc48aa73694122edae77e6fe97fafbfbe.cdn.bubble.io%2Ff1719160628044x521595994976241200%2F%25E1%2584%2587%25E1%2585%25A9%25E1%2584%258B%25E1%2585%25B5%25E1%2586%25AB%25E1%2584%2580%25E1%2585%25A9%25205-22-24.jpg?w=768&h=&auto=compress&dpr=1.25&fit=max',
   },
   {
+    id: uuidv4(),
     school: '보인고',
     date: '2024-05-22',
     url: 'https://d1muf25xaso8hp.cloudfront.net/https%3A%2F%2Fc48aa73694122edae77e6fe97fafbfbe.cdn.bubble.io%2Ff1719160647595x857826965046250200%2F%25E1%2584%2587%25E1%2585%25A9%25E1%2584%258B%25E1%2585%25B5%25E1%2586%25AB%25E1%2584%2580%25E1%2585%25A9%25205-22-24%2520%25282%2529.jpg?w=768&h=&auto=compress&dpr=1.25&fit=max',
   },
   {
+    id: uuidv4(),
     school: '한백고',
     date: '2024-05-15',
     url: 'https://d1muf25xaso8hp.cloudfront.net/https%3A%2F%2Fc48aa73694122edae77e6fe97fafbfbe.cdn.bubble.io%2Ff1719160526956x516255420202004000%2F%25E1%2584%2592%25E1%2585%25A1%25E1%2586%25AB%25E1%2584%2587%25E1%2585%25A2%25E1%2586%25A8%25E1%2584%2580%25E1%2585%25A9%25201.jpg?w=768&h=&auto=compress&dpr=1.25&fit=max',
   },
   {
+    id: uuidv4(),
     school: '동명외고',
     date: '2024-05-10',
     url: 'https://d1muf25xaso8hp.cloudfront.net/https%3A%2F%2Fc48aa73694122edae77e6fe97fafbfbe.cdn.bubble.io%2Ff1719160479497x884936292665685900%2F%25E1%2584%2583%25E1%2585%25A9%25E1%2586%25BC%25E1%2584%2586%25E1%2585%25A7%25E1%2586%25BC%25E1%2584%258B%25E1%2585%25A7%25E1%2584%2580%25E1%2585%25A9%2520%25281%2529.jpg?w=768&h=&auto=compress&dpr=1.25&fit=max',
   },
   {
+    id: uuidv4(),
     school: '계성고',
     date: '2024-05-10',
     url: 'https://d1muf25xaso8hp.cloudfront.net/https%3A%2F%2Fc48aa73694122edae77e6fe97fafbfbe.cdn.bubble.io%2Ff1719160569144x168738590511048340%2F%25E1%2584%2580%25E1%2585%25A8%25E1%2584%2589%25E1%2585%25A5%25E1%2586%25BC%25E1%2584%2580%25E1%2585%25A9%25201.jpg?w=768&h=&auto=compress&dpr=1.25&fit=max',
   },
   {
+    id: uuidv4(),
     school: '계성고',
     date: '2024-05-10',
     url: 'https://d1muf25xaso8hp.cloudfront.net/https%3A%2F%2Fc48aa73694122edae77e6fe97fafbfbe.cdn.bubble.io%2Ff1719160592227x952193130158788900%2F%25E1%2584%2580%25E1%2585%25A8%25E1%2584%2589%25E1%2585%25A5%25E1%2586%25BC%25E1%2584%2580%25E1%2585%25A9%25202%2520%25281%2529.jpg?w=768&h=&auto=compress&dpr=1.25&fit=max',
   },
   {
+    id: uuidv4(),
     school: '고양외고',
     date: '2024-04-03',
     url: 'https://d1muf25xaso8hp.cloudfront.net/https%3A%2F%2Fc48aa73694122edae77e6fe97fafbfbe.cdn.bubble.io%2Ff1719160400352x792852318610204200%2F%25E1%2584%2580%25E1%2585%25A9%25E1%2584%258B%25E1%2585%25A3%25E1%2586%25BC%25E1%2584%258B%25E1%2585%25AC%25E1%2584%2580%25E1%2585%25A9%25201%2520%25281%2529.jpg?w=768&h=&auto=compress&dpr=1.25&fit=max',
   },
   {
+    id: uuidv4(),
     school: '고양외고',
     date: '2024-04-02',
     url: 'https://d1muf25xaso8hp.cloudfront.net/https%3A%2F%2Fc48aa73694122edae77e6fe97fafbfbe.cdn.bubble.io%2Ff1719160435368x888459019327817700%2F%25E1%2584%2580%25E1%2585%25A9%25E1%2584%258B%25E1%2585%25A3%25E1%2586%25BC%25E1%2584%258B%25E1%2585%25AC%25E1%2584%2580%25E1%2585%25A9%25202.jpg?w=768&h=&auto=compress&dpr=1.25&fit=max',
   },
   {
+    id: uuidv4(),
     school: '대화고',
     date: '2022-11-01',
     url: 'https://d1muf25xaso8hp.cloudfront.net/https%3A%2F%2Fc48aa73694122edae77e6fe97fafbfbe.cdn.bubble.io%2Ff1719160311813x941715597763507200%2F%25E1%2584%2583%25E1%2585%25A2%25E1%2584%2592%25E1%2585%25AA%25E1%2584%2580%25E1%2585%25A9%252022-11-01%2528%25E1%2584%2589%25E1%2585%25AE%25E1%2584%258C%25E1%2585%25A5%25E1%2586%25BC%2529.jpg?w=768&h=&auto=compress&dpr=1.25&fit=max',
   },
   {
+    id: uuidv4(),
     school: '한영고',
     date: '2022-08-22',
     url: 'https://d1muf25xaso8hp.cloudfront.net/https%3A%2F%2Fc48aa73694122edae77e6fe97fafbfbe.cdn.bubble.io%2Ff1719159991545x757471049248652300%2F%25E1%2584%2592%25E1%2585%25A1%25E1%2586%25AB%25E1%2584%258B%25E1%2585%25A7%25E1%2586%25BC%25E1%2584%2580%25E1%2585%25A9%252022-8-22%2528%25E1%2584%2589%25E1%2585%25AE%25E1%2584%258C%25E1%2585%25A5%25E1%2586%25BC%2529.jpg?w=768&h=&auto=compress&dpr=1.25&fit=max',
   },
   {
+    id: uuidv4(),
     school: '한영고',
     date: '2022-08-22',
     url: 'https://d1muf25xaso8hp.cloudfront.net/https%3A%2F%2Fc48aa73694122edae77e6fe97fafbfbe.cdn.bubble.io%2Ff1719160020255x733308881895276200%2F%25E1%2584%2592%25E1%2585%25A1%25E1%2586%25AB%25E1%2584%258B%25E1%2585%25A7%25E1%2586%25BC%25E1%2584%2580%25E1%2585%25A9%25202022-8-22%2528%25E1%2584%2589%25E1%2585%25AE%25E1%2584%258C%25E1%2585%25A5%25E1%2586%25BC%2529.jpg?w=768&h=&auto=compress&dpr=1.25&fit=max',
   },
   {
+    id: uuidv4(),
     school: '한영고',
     date: '2022-08-22',
     url: 'https://d1muf25xaso8hp.cloudfront.net/https%3A%2F%2Fc48aa73694122edae77e6fe97fafbfbe.cdn.bubble.io%2Ff1719160081604x771879645990818800%2F%25E1%2584%2592%25E1%2585%25A1%25E1%2586%25AB%25E1%2584%258B%25E1%2585%25A7%25E1%2586%25BC%25E1%2584%2580%25E1%2585%25A9%252022-8-22%25281%2529%2528%25E1%2584%2589%25E1%2585%25AE%25E1%2584%258C%25E1%2585%25A5%25E1%2586%25BC%2529.jpg?w=768&h=&auto=compress&dpr=1.25&fit=max',
   },
   {
+    id: uuidv4(),
     school: '여의도고',
     date: '2022-07-17',
     url: 'https://d1muf25xaso8hp.cloudfront.net/https%3A%2F%2Fc48aa73694122edae77e6fe97fafbfbe.cdn.bubble.io%2Ff1719159900093x382748128604680100%2F%25E1%2584%258B%25E1%2585%25A7%25E1%2584%258B%25E1%2585%25B4%25E1%2584%2583%25E1%2585%25A9%25E1%2584%2580%25E1%2585%25A9%252022-7-17.jpg?w=768&h=&auto=compress&dpr=1.25&fit=max',
   },
   {
+    id: uuidv4(),
     school: '여의도고',
     date: '2022-07-14',
     url: 'https://d1muf25xaso8hp.cloudfront.net/https%3A%2F%2Fc48aa73694122edae77e6fe97fafbfbe.cdn.bubble.io%2Ff1719159840242x487020587534231500%2F%25E1%2584%258B%25E1%2585%25A7%25E1%2584%258B%25E1%2585%25B4%25E1%2584%2583%25E1%2585%25A9%25E1%2584%2580%25E1%2585%25A9%252022-7-14%25281%2529.jpg?w=768&h=&auto=compress&dpr=1.25&fit=max',
   },
   {
+    id: uuidv4(),
     school: '여의도고',
     date: '2022-07-14',
     url: 'https://d1muf25xaso8hp.cloudfront.net/https%3A%2F%2Fc48aa73694122edae77e6fe97fafbfbe.cdn.bubble.io%2Ff1719159869122x681833420345764100%2F%25E1%2584%258B%25E1%2585%25A7%25E1%2584%258B%25E1%2585%25B4%25E1%2584%2583%25E1%2585%25A9%25E1%2584%2580%25E1%2585%25A9%252022-7-14.jpg?w=768&h=&auto=compress&dpr=1.25&fit=max',
   },
 
   {
+    id: uuidv4(),
     school: '계성고',
     date: '2022-07-07',
     url: 'https://d1muf25xaso8hp.cloudfront.net/https%3A%2F%2Fc48aa73694122edae77e6fe97fafbfbe.cdn.bubble.io%2Ff1719159934113x356374731330934900%2F%25E1%2584%2580%25E1%2585%25A8%25E1%2584%2589%25E1%2585%25A5%25E1%2586%25BC%25E1%2584%2580%25E1%2585%25A9%252022-7-7%2528%25E1%2584%2589%25E1%2585%25AE%25E1%2584%258C%25E1%2585%25A5%25E1%2586%25BC%2529%2520.jpg?w=768&h=&auto=compress&dpr=1.25&fit=max',
   },
   {
+    id: uuidv4(),
     school: '고척고',
     date: '2022-06-03',
     url: 'https://d1muf25xaso8hp.cloudfront.net/https%3A%2F%2Fc48aa73694122edae77e6fe97fafbfbe.cdn.bubble.io%2Ff1719159710725x688826155779622700%2F%25E1%2584%2580%25E1%2585%25A9%25E1%2584%258E%25E1%2585%25A5%25E1%2586%25A8%25E1%2584%2580%25E1%2585%25A9%252022-6-3%2520%25281%2529.jpg?w=768&h=&auto=compress&dpr=1.25&fit=max',
   },
   {
+    id: uuidv4(),
     school: '고척고',
     date: '2022-06-03',
     url: 'https://d1muf25xaso8hp.cloudfront.net/https%3A%2F%2Fc48aa73694122edae77e6fe97fafbfbe.cdn.bubble.io%2Ff1719159792263x486260511720529860%2F%25E1%2584%2580%25E1%2585%25A9%25E1%2584%258E%25E1%2585%25A5%25E1%2586%25A8%25E1%2584%2580%25E1%2585%25A9%252022-6-3.jpg?w=768&h=&auto=compress&dpr=1.25&fit=max',
