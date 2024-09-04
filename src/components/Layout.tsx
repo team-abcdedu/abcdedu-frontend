8b493fd037f898cca6cc017ba7e698932c16ec01
mihee <s2809h29@gmail.com>
2024-09-04 22:08:06 +0900

    feat: 페이지 이동 시 스크롤 위치 초기화

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,9 +1,16 @@
-import { Outlet } from 'react-router-dom';
+import { useEffect } from 'react';
+import { Outlet, useLocation } from 'react-router-dom';
 
 import Footer from '@/components/Footer/index';
 import Header from '@/components/Header';
 
 function Layout() {
+  const location = useLocation();
+
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, [location.pathname]);
+
   return (
     <>
       <Header />
