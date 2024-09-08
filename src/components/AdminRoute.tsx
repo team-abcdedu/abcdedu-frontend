5ccb5e0cbbc88aea25f74a3efc27ebc60d35253c
PHJoon <phj9503@gmail.com>
2024-09-08 20:42:35 +0900

    feat: 관리자 권한 체크 추가

--- src/components/AdminRoute.tsx
+++ src/components/AdminRoute.tsx
@@ -1,8 +1,21 @@
-import { PropsWithChildren } from 'react';
+import { PropsWithChildren, useEffect } from 'react';
 import { Navigate } from 'react-router-dom';
 
+import useBoundStore from '@/stores';
+
 export default function AdminRoute({ children }: PropsWithChildren) {
-  // role check
-  const admin = true;
-  return admin ? children : <Navigate to={'/'} replace />;
+  const user = useBoundStore(state => state.user);
+
+  useEffect(() => {
+    // todo: 나중에 관리자로 변경
+    if (user && user.role !== '새싹') {
+      alert('권한이 없습니다.');
+    }
+  }, [user]);
+
+  return user && user.role === '새싹' ? (
+    children
+  ) : (
+    <Navigate to={'/'} replace />
+  );
 }
