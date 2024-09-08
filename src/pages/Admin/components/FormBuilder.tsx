5fb08e02d8c666617e56296afac41d7938f65395
PHJoon <phj9503@gmail.com>
2024-09-09 04:56:28 +0900

    feat: formBuilder 추가

---
+++ src/pages/Admin/components/FormBuilder.tsx
@@ -0,0 +1,52 @@
+import { useState } from 'react';
+
+import QuestionnaireBuilder from '@/pages/Admin/components/QuestionnaireBuilder';
+
+interface InputItem {
+  id: number;
+}
+
+function FormBuilder() {
+  const [inputItems, setInputItems] = useState<InputItem[]>([{ id: 0 }]);
+  const [inputIdx, setInputIdx] = useState(1);
+
+  const handleAddItem = () => {
+    setInputItems([...inputItems, { id: inputIdx }]);
+    setInputIdx(prev => prev + 1);
+  };
+
+  const handleDeleteItem = (index: number) => {
+    setInputItems(inputItems.filter(item => item.id !== index));
+  };
+
+  return (
+    <form className={'w-full h-full flex flex-col gap-10 overflow-hidden'}>
+      <ul
+        className={
+          'w-full h-[calc(100%_-_50px)] p-5 flex flex-col items-center gap-5 border-3 rounded-xl overflow-y-scroll bg-neutral-100'
+        }
+      >
+        {inputItems.map(item => (
+          <QuestionnaireBuilder
+            key={item.id}
+            deleteHandler={() => handleDeleteItem(item.id)}
+          />
+        ))}
+        <button
+          type={'button'}
+          onClick={handleAddItem}
+          className={'w-1/2 flex-row-center rounded-lg bg-neutral-300'}
+        >
+          +
+        </button>
+      </ul>
+      <button
+        className={'h-40 px-20 text-20 border-2 rounded-lg border-neutral-300'}
+      >
+        등록하기
+      </button>
+    </form>
+  );
+}
+
+export default FormBuilder;
---
+++ src/pages/Admin/components/QuestionnaireInput.tsx
@@ -0,0 +1,59 @@
+import { useState } from 'react';
+
+interface FlexibleRadioInputItem {
+  id: number;
+}
+
+interface FlexibleRadioInputProps {
+  type: 'radio' | 'checkbox' | 'textarea';
+}
+
+function QuestionnaireInput({ type }: FlexibleRadioInputProps) {
+  const textareaStyle = type === 'textarea' ? 'hidden' : 'block';
+
+  const [items, setItems] = useState<FlexibleRadioInputItem[]>([{ id: 0 }]);
+  const [itemIdx, setItemIdx] = useState(1);
+
+  const handleAddItem = () => {
+    setItems([...items, { id: itemIdx }]);
+    setItemIdx(prev => prev + 1);
+  };
+
+  const handleDeleteItem = (index: number) => {
+    setItems(items.filter((_, i) => i !== index));
+  };
+
+  return (
+    <div className={'flex-col-center gap-3'}>
+      {items.map(item => (
+        <div key={item.id} className={'w-full flex justify-between'}>
+          <div className={'w-[10%] flex-row-center gap-20'}>
+            {type === 'radio' && <input name={'temp'} type='radio' />}
+            {type === 'checkbox' && <input name={'temp'} type='checkbox' />}
+            <button
+              type={'button'}
+              onClick={() => handleDeleteItem(itemIdx)}
+              className={`${textareaStyle} text-13 text-red-700 font-semibold`}
+            >
+              X
+            </button>
+          </div>
+          <input
+            type='text'
+            className={`${textareaStyle} w-[85%] px-10 border-1 rounded-md`}
+            placeholder={'내용을 입력해주세요'}
+          />
+        </div>
+      ))}
+      <button
+        type={'button'}
+        className={`${textareaStyle} w-1/5 px-20 text-13 font-semibold border-2 rounded-2xl`}
+        onClick={handleAddItem}
+      >
+        +
+      </button>
+    </div>
+  );
+}
+
+export default QuestionnaireInput;
