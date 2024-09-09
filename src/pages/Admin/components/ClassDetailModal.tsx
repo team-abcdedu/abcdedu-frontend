872eb100ef7821de1e47921402bd41a894e0b98d
PHJoon <phj9503@gmail.com>
2024-09-09 09:37:42 +0900

    feat: classDetailModal 내부 수정

--- src/pages/Admin/components/ClassDetailModal.tsx
+++ src/pages/Admin/components/ClassDetailModal.tsx
@@ -1,5 +1,3 @@
-import { useEffect } from 'react';
-
 import Modal from '@/components/Modal';
 
 import { ClassTableData } from '../types';
@@ -17,7 +15,8 @@ function ClassDetailModal({
   isVisible,
   onClose,
 }: ClassDetailModalProps) {
-  useEffect(() => {}, []);
+  const spanStyle = 'text-14 text-neutral-500 whitespace-nowrap';
+  const subClassItemWrapperStyle = 'flex items-center gap-30';
 
   if (!classData) return null;
 
@@ -26,14 +25,58 @@ function ClassDetailModal({
       <Modal.Content>
         <div className={'h-max flex flex-col gap-5'}>
           <DetailModalHeader
-            list={{ ID: classData.id, 클래스명: classData.title }}
+            list={{ 클래스명: classData.title, 타입: classData.type }}
           />
-          <div
-            className={'h-[400px] p-5 border-2 border-neutral-300 rounded-sm'}
+          <fieldset className={'w-full border-2 border-neutral-300 rounded-sm'}>
+            <legend className={'text-14 text-neutral-500 text-center'}>
+              클래스 설명
+            </legend>
+            <textarea
+              className={'w-full py-5 px-10 text-15'}
+              value={classData.description}
+              readOnly
+            />
+          </fieldset>
+          <fieldset
+            className={
+              'h-[400px] p-3 pl-8 border-2 border-neutral-300 rounded-sm overflow-hidden'
+            }
           >
-            하위클래스 목록: {classData.list.join(', ')}
-            설명 : {classData.description}
-          </div>
+            <legend className={'text-14 text-neutral-500 text-center'}>
+              서브 클래스
+            </legend>
+            <ul
+              className={'w-full h-full flex flex-col gap-5 overflow-y-scroll'}
+            >
+              {classData.subClasses.map(subClass => (
+                <li
+                  key={subClass.subClassId}
+                  className={
+                    'w-full max-h-full py-5 px-10 grid grid-cols-2 border-2 bg-neutral-50 rounded-md'
+                  }
+                >
+                  <div className={`${subClassItemWrapperStyle}`}>
+                    <span className={`${spanStyle}`}>ID</span>
+                    <span>{subClass.subClassId}</span>
+                  </div>
+                  <div className={`${subClassItemWrapperStyle}`}>
+                    <span className={`${spanStyle}`}>순서</span>
+                    <span>{subClass.orderNumber}</span>
+                  </div>
+                  <div className={`col-span-2 ${subClassItemWrapperStyle}`}>
+                    <span className={`${spanStyle}`}>서브 클래스명</span>
+                    <span className={'w-full break-all'}>{subClass.title}</span>
+                  </div>
+                  <div className={`col-span-2 ${subClassItemWrapperStyle}`}>
+                    <span className={`${spanStyle}`}>설명</span>
+                    <span className={'w-full break-all'}>
+                      {subClass.description}
+                    </span>
+                  </div>
+                </li>
+              ))}
+            </ul>
+          </fieldset>
         </div>
       </Modal.Content>
       <Modal.Actions>
