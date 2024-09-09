2902a3b83b5f984119337cb39db6e6d2e7754a56
PHJoon <phj9503@gmail.com>
2024-09-09 09:38:04 +0900

    feat: class 등록 후 alert 추가

--- src/pages/Admin/hooks/useClass.ts
+++ src/pages/Admin/hooks/useClass.ts
@@ -18,9 +18,12 @@ function useClass() {
       type: string;
       description: string;
     }) => AdminClassApi.createClass(classData),
-    onSuccess: () =>
-      queryClient.invalidateQueries({ queryKey: ['admin-class'] }),
+    onSuccess: () => {
+      alert('클래스가 등록되었습니다.');
+      queryClient.invalidateQueries({ queryKey: ['admin-class'] });
+    },
     onError: error => {
+      alert('클래스 등록에 실패했습니다.');
       if (isAxiosError(error)) {
         console.error(error.response?.data.result.message);
         return;
@@ -36,9 +39,12 @@ function useClass() {
       description: string;
       orderNumber: number;
     }) => AdminClassApi.createSubClass(subClassData),
-    onSuccess: () =>
-      queryClient.invalidateQueries({ queryKey: ['admin-class'] }),
+    onSuccess: () => {
+      alert('서브 클래스가 등록되었습니다.');
+      queryClient.invalidateQueries({ queryKey: ['admin-class'] });
+    },
     onError: error => {
+      alert('서브 클래스 등록에 실패했습니다.');
       if (isAxiosError(error)) {
         console.error(error.response?.data.result.message);
         return;
