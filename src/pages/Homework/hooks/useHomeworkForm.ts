1a7dd992d4fb7fb3e27cf90c2885809b2fdaa2e8
PHJoon <phj9503@gmail.com>
2024-09-13 17:58:11 +0900

    feat: form 커스텀 훅 추가

---
+++ src/pages/Homework/hooks/useHomeworkForm.ts
@@ -0,0 +1,79 @@
+import { useMutation, useQueryClient } from '@tanstack/react-query';
+import { SubmitHandler, useForm } from 'react-hook-form';
+
+import HomeworkApi from '@/services/homework';
+import { MyHomeworkAnswerInfo, QuestionInfo } from '@/types/homework';
+
+interface IHomeworkForm {
+  [key: number]: string | string[];
+}
+
+interface UseHomeworkFormProps {
+  homeworkId: number;
+  questions: QuestionInfo[];
+  setSuccessModal: (value: boolean) => void;
+  toggleModal: () => void;
+}
+
+function useHomeworkForm({ homeworkId, questions }: UseHomeworkFormProps) {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+    reset,
+  } = useForm<IHomeworkForm>({ mode: 'onSubmit' });
+
+  const queryClient = useQueryClient();
+
+  const mutation = useMutation({
+    mutationFn: (data: MyHomeworkAnswerInfo[]) =>
+      HomeworkApi.postMyHomework(homeworkId, data),
+    onSuccess: () => {
+      queryClient.invalidateQueries(['my-homework', { homeworkId }]);
+      reset();
+      setSuccessModal(true);
+      toggleModal();
+    },
+    onError: error => {
+      console.error(error);
+      setSuccessModal(false);
+      toggleModal();
+    },
+  });
+
+  const submitHandler: SubmitHandler<IHomeworkForm> = (
+    data: IHomeworkForm,
+    e,
+  ) => {
+    e?.preventDefault();
+
+    const refinedData = Object.entries(data).map(([key, value]) => {
+      const questionId = Number(key);
+
+      if (questions[questionId - 1].type === 'MULTIPLE_OPTION') {
+        return {
+          questionId,
+          optionIndexes: value.map(v => Number(v)),
+        };
+      }
+      if (questions[questionId - 1].type === 'SINGLE_OPTION') {
+        return {
+          questionId,
+          optionIndex: Number(value),
+        };
+      }
+      return {
+        questionId,
+        content: value as string,
+      };
+    });
+
+    mutation.mutate(refinedData);
+  };
+
+  const onSubmit = handleSubmit(submitHandler);
+
+  return { register, errors, reset, onSubmit };
+}
+
+export default useHomeworkForm;
