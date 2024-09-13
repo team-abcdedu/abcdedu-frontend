e75137567e29407afab02c090f417d7067957cf2
PHJoon <phj9503@gmail.com>
2024-09-13 17:57:49 +0900

    feat: HomeworkForm 수정

---
+++ src/pages/Homework/components/HomeworkForm.tsx
@@ -0,0 +1,120 @@
+import { useEffect, useState } from 'react';
+
+import MessageModal from '@/components/MessageModal';
+import useModal from '@/hooks/useModal';
+import UserInput from '@/pages/Homework/components/UserInput';
+import { mockAnswer } from '@/pages/Homework/constants';
+// import useGetMyHomework from '@/pages/Homework/hooks/useGetMyHomework';
+import useHomeworkForm from '@/pages/Homework/hooks/useHomeworkForm';
+import { QuestionInfo } from '@/types/homework';
+
+interface HomeworkFormProps {
+  homeworkId: number;
+  questions: QuestionInfo[];
+  additionalDescription: string;
+  showScore: boolean;
+}
+
+function HomeworkForm({
+  homeworkId,
+  questions,
+  additionalDescription,
+  showScore,
+}: HomeworkFormProps) {
+  // const { data: myAnswers, isLoading, isError } = useGetMyHomework({ homeworkId });
+  const isError = false;
+  const isLoading = false;
+  const myAnswers = mockAnswer;
+
+  const { isVisible, toggleModal } = useModal();
+  const [successModal, setSuccessModal] = useState<boolean>(false);
+
+  const formTextStyle = 'text-16 md:text-20 whitespace-pre-wrap';
+  const buttonBgStyle =
+    myAnswers && myAnswers.length > 0 ? 'bg-neutral-300' : 'bg-primary-300';
+
+  const { register, errors, reset, onSubmit } = useHomeworkForm({
+    homeworkId,
+    questions,
+    setSuccessModal,
+    toggleModal,
+  });
+
+  useEffect(() => {
+    reset();
+  }, [reset]);
+
+  if (isError || isLoading) {
+    return (
+      <div
+        className={
+          'w-full h-[600px] flex-row-center text-center text-30 bg-neutral-100'
+        }
+      >
+        {isError ? '에러가 발생했습니다.' : '로딩 중입니다.'}
+      </div>
+    );
+  }
+
+  return (
+    <>
+      <form
+        className={`w-full h-max mb-[40px] py-[70px] px-[50px] md:py-[100px] md:px-[170px] flex-col-center gap-70 self-center bg-neutral-100 ${formTextStyle}`}
+        onSubmit={onSubmit}
+      >
+        <div className={'min-w-[140px] md:min-w-[700px] flex flex-col gap-20'}>
+          <div className={'py-30 flex flex-col gap-40'}>
+            {questions.map(question => (
+              <div
+                key={question.index}
+                className={'w-full flex flex-col gap-30'}
+              >
+                <div className={`w-full flex flex-col gap-20`}>
+                  <div className={'font-semibold'}>
+                    {question.index}. {question.title}
+                    <span className={'text-14 text-red-700'}>
+                      {showScore && ` (${question.score}점)`}
+                    </span>
+                  </div>
+                  <div className={'pl-10'}>{question.description}</div>
+                </div>
+                <div className={`w-full`}>
+                  <UserInput
+                    question={question}
+                    answer={myAnswers[question.index - 1]}
+                    register={register}
+                    errors={errors}
+                  />
+                  {/* <UserInput q={q} answer={null} register={register} errors={errors} /> */}
+                </div>
+              </div>
+            ))}
+          </div>
+
+          <div className={`w-full flex flex-col gap-20`}>
+            <h3 className={'font-semibold'}>[토론 및 발표]</h3>
+            <p className={'indent-[20px] whitespace-pre-wrap'}>
+              {additionalDescription}
+            </p>
+          </div>
+
+          <button
+            className={`min-w-[150px] min-h-[50px] mt-30 self-center rounded-[10px] text-white ${buttonBgStyle}`}
+            disabled={myAnswers.length > 0}
+          >
+            제출하기
+          </button>
+        </div>
+      </form>
+
+      <MessageModal
+        isVisible={isVisible}
+        onClose={toggleModal}
+        type={successModal ? 'success' : 'error'}
+        message={successModal ? '제출되었습니다.' : '제출에 실패했습니다.'}
+      />
+    </>
+  );
+}
+
+export default HomeworkForm;
---
+++ src/pages/Homework/components/UserInput.tsx
@@ -0,0 +1,105 @@
+import { FieldErrors } from 'react-hook-form';
+
+import { MyHomeworkAnswerInfo, QuestionInfo } from '@/types/homework';
+
+interface UserInputAreaProps {
+  question: QuestionInfo;
+  answer: MyHomeworkAnswerInfo | null;
+  register: (name: string, RegisterOptions?) => { onChange; onBlur; name; ref };
+  errors: FieldErrors;
+}
+
+function ErrorMessage({
+  errors,
+  index,
+}: {
+  errors: FieldErrors;
+  index: number;
+}) {
+  return (
+    errors?.[`${index}`] && (
+      <span className={'text-14 text-red-700'}>답안을 입력해주세요</span>
+    )
+  );
+}
+
+function UserInput({ question, answer, register, errors }: UserInputAreaProps) {
+  const inputTextStyle = answer ? 'text-neutral-300' : '';
+
+  if (question.type === 'SUBJECTIVE') {
+    return (
+      <>
+        <textarea
+          {...register(`${question.index}`, { required: true })}
+          className={`w-full min-h-[200px] max-h-[200px] p-10 ${inputTextStyle}`}
+          placeholder={'답안을 입력해주세요'}
+          defaultValue={answer?.content ?? ''}
+          readOnly={!!answer}
+        />
+        <ErrorMessage errors={errors} index={question.index} />
+      </>
+    );
+  }
+
+  if (question.type === 'SHORT_ANSWER') {
+    return (
+      <>
+        <input
+          {...register(`${question.index}`, { required: true })}
+          className={`w-full p-10 ${inputTextStyle}`}
+          placeholder={'답안을 입력해주세요'}
+          defaultValue={answer?.content ?? ''}
+          readOnly={!!answer}
+        />
+        <ErrorMessage errors={errors} index={question.index} />
+      </>
+    );
+  }
+
+  if (question.type === 'MULTIPLE_OPTION') {
+    return (
+      <>
+        <div className={'flex gap-20'}>
+          {question.options?.map(option => (
+            <label key={option.index} className={'flex gap-10 font-light'}>
+              <input
+                {...register(`${question.index}`, { required: true })}
+                type={'checkbox'}
+                value={option.index}
+                disabled={!!answer}
+                checked={answer?.optionIndexes.includes(option.index)}
+              />
+              {option.content}
+            </label>
+          ))}
+        </div>
+        <ErrorMessage errors={errors} index={question.index} />
+      </>
+    );
+  }
+
+  if (question.type === 'SINGLE_OPTION') {
+    return (
+      <>
+        <div className={'flex gap-20'}>
+          {question.options?.map(option => (
+            <label key={option.index} className={'flex gap-10 font-light'}>
+              <input
+                {...register(`${question.index}`, { required: true })}
+                type={'radio'}
+                value={option.index}
+                disabled={!!answer}
+                defaultChecked={answer?.optionIndex === option.index}
+              />
+              {option.content}
+            </label>
+          ))}
+        </div>
+        <ErrorMessage errors={errors} index={question.index} />
+      </>
+    );
+  }
+  return null;
+}
+
+export default UserInput;
