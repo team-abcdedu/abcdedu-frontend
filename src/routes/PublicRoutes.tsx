12fd3fd416dc33f8f14e9b5f864cf0330b7429b5
PHJoon <phj9503@gmail.com>
2024-09-14 18:29:06 +0900

    fix: route 파일 경로 수정

--- src/routes/PublicRoutes.tsx
+++ src/routes/PublicRoutes.tsx
@@ -19,7 +19,7 @@ const PostDetail = lazy(() => import('@/pages/Community/PostDetail'));
 const Project = lazy(() => import('@/pages/Community/Project'));
 const Qna = lazy(() => import('@/pages/Community/Qna'));
 
-const Assignment = lazy(() => import('@/pages/Assignment'));
+const Homework = lazy(() => import('@/pages/Homework'));
 const Survey = lazy(() => import('@/pages/Survey'));
 
 const Contact = lazy(() => import('@/pages/Contact'));
