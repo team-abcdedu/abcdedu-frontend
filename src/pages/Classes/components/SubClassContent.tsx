7aaf85e1b5826d73b8024adcacc18a4d56c183c3
PHJoon <phj9503@gmail.com>
2024-09-16 11:03:28 +0900

    feat: SubClass 자료 추가 및 다운로드 로직 추가

--- src/pages/Classes/components/SubClassContent.tsx
+++ src/pages/Classes/components/SubClassContent.tsx
@@ -2,54 +2,94 @@ import { useEffect, useState } from 'react';
 
 import Book from '@/assets/icons/book.svg?react';
 import CheckToSlot from '@/assets/icons/check-to-slot.svg?react';
+import Paperclip from '@/assets/icons/paperclip.svg?react';
 import MessageModal from '@/components/MessageModal';
+import useGetSubClassFileList from '@/hooks/class/useGetSubClassFileList';
+import useGetSubClassGeneralFile from '@/hooks/class/useGetSubClassGeneralFile';
 import useModal from '@/hooks/useModal';
+import ExamContent from '@/pages/Classes/components/ExamContent';
+import { SubClassContentState } from '@/pages/Classes/types';
+import useBoundStore from '@/stores';
 
-import { tempExamInfoMap } from '../constants';
-
-// import ExamForm from './ExamForm';
-
-function SubClassContent({
-  subClassCode,
-  subClassTitle,
-}: {
-  subClassCode: string;
-  subClassTitle: string;
-}) {
+function SubClassContent({ subClassId }: { subClassId: number }) {
   const buttonStyle =
     'w-100 min-h-[140px] flex flex-col justify-start items-center place-self-center';
   const iconWrapperStyle = 'w-100 h-100 flex-row-center';
   const iconStyle = 'w-70 h-70 sm:w-90 sm:h-90 text-primary-300';
   const textStyle = 'text-20 sm:text-25 font-semibold text-center';
+
   const [modalMessage, setModalMessage] = useState('');
-
-  // form exam
-  // const examInfo = examInfoMap[subClassCode];
-
-  const examInfo = tempExamInfoMap[subClassCode];
-
   const { isVisible, toggleModal } = useModal();
 
+  const [fileUrl, setFileUrl] = useState<string | null>(null);
   const [openExam, setOpenExam] = useState(false);
+  const [contentState, setContentState] = useState<SubClassContentState>({
+    generalType: null,
+    generalFileId: null,
+  });
 
-  const handleTheoryClick = () => {
-    // 권한 체크
-    setModalMessage('이론 자료는 관리자만 이용 가능합니다.');
-    toggleModal();
+  const { data: fileList } = useGetSubClassFileList({
+    subLectureId: subClassId,
+  });
+  const { data: generalFile } = useGetSubClassGeneralFile({
+    assignmentFileId: contentState?.generalFileId,
+  });
+  const { user } = useBoundStore();
+
+  const findFile = (type: string) => {
+    return fileList?.find(file => file.assignmentType === type);
   };
 
-  const handleExamClick = () => {
-    if (examInfo) {
-      setOpenExam(prev => !prev);
+  const handleDownloadFile = (type: '이론' | '자료') => {
+    if (user?.role === '관리자') {
+      const file = findFile(type);
+      if (!file) {
+        setModalMessage(`${type} 파일이 없습니다.`);
+        toggleModal();
+        return;
+      }
+      setContentState({
+        generalType: type,
+        generalFileId: file.assignmentFileId,
+      });
     } else {
-      setModalMessage('시험 정보가 없습니다.');
+      setModalMessage('관리자만 이용 가능합니다.');
       toggleModal();
     }
   };
 
+  const handleExamClick = () => {
+    const file = findFile('시험');
+    if (!file) {
+      setModalMessage('시험 정보가 없습니다.');
+      toggleModal();
+      return;
+    }
+    setContentState({
+      generalType: '시험',
+      generalFileId: file.assignmentFileId,
+    });
+    setOpenExam(prev => !prev);
+  };
+
   useEffect(() => {
     setOpenExam(false);
-  }, [subClassCode, subClassTitle]);
+  }, [subClassId]);
+
+  useEffect(() => {
+    if (generalFile) {
+      setFileUrl(generalFile.filePresignedUrl);
+    }
+    if (
+      contentState.generalType === '이론' ||
+      contentState.generalType === '자료'
+    ) {
+      const linkEle = document.createElement('a');
+      linkEle.href = generalFile?.filePresignedUrl ?? '';
+      linkEle.download = `${contentState.generalType}.pdf`;
+      linkEle.click();
+    }
+  }, [generalFile, contentState]);
 
   return (
     <>
@@ -58,11 +98,24 @@ function SubClassContent({
           'mt-0 mb-40 sm:mt-30 sm:mb-100 px-50 grid grid-cols-2 sm:flex-row-center gap-20 sm:gap-50 '
         }
       >
-        <button className={buttonStyle} onClick={handleTheoryClick}>
+        <button
+          className={buttonStyle}
+          onClick={() => handleDownloadFile('이론')}
+        >
           <div className={iconWrapperStyle}>
             <Book className={iconStyle} />
           </div>
           <div className={textStyle}>이론</div>
+        </button>
+
+        <button
+          className={buttonStyle}
+          onClick={() => handleDownloadFile('자료')}
+        >
+          <div className={iconWrapperStyle}>
+            <Paperclip className={iconStyle} />
+          </div>
+          <div className={textStyle}>자료</div>
         </button>
 
         <button className={buttonStyle} onClick={handleExamClick}>
@@ -75,7 +128,7 @@ function SubClassContent({
 
       <MessageModal
         isVisible={isVisible}
-        onClose={handleTheoryClick}
+        onClose={toggleModal}
         type={'error'}
         message={modalMessage}
       />
@@ -83,35 +136,8 @@ function SubClassContent({
       {/* {openExam && examInfo && <ExamForm examInfo={examInfo} />} */}
 
       {/* pdf, hwp exam */}
-      {openExam && examInfo && (
-        <div className={'w-full'}>
-          <div className={'w-full flex-col-center sm:flex-row-center gap-30'}>
-            <a
-              href={examInfo.pdf}
-              target={'_blank'}
-              className={
-                'p-10 text-20 border-2 border-primary-300 rounded-lg hover:bg-primary-300 hover:text-white transition ease-in-out delay-50'
-              }
-              rel='noreferrer'
-            >
-              pdf 새 탭에서 열기
-            </a>
-            <a
-              href={examInfo.hwp}
-              download={`${subClassCode}-[이름]`}
-              className={
-                'p-10 text-20 border-2 border-primary-300 rounded-lg hover:bg-primary-300 hover:text-white transition ease-in-out delay-50'
-              }
-            >
-              과제 제출용 hwp 다운로드
-            </a>
-          </div>
-          <iframe
-            src={examInfo.pdf}
-            title={'exam-pdf'}
-            className={'w-full h-[600px] md:h-[1000px] p-40 md:p-80'}
-          ></iframe>
-        </div>
+      {openExam && (
+        <ExamContent pdfUrl={fileUrl} pdfFileId={contentState.generalFileId} />
       )}
     </>
   );
