45902e515bad784e9be0647a69a59c077d41f96a
mihee <s2809h29@gmail.com>
2024-09-17 22:01:12 +0900

    refactor: 게시글 상세 페이지 리팩토링 #113

--- src/pages/Community/PostDetail.tsx
+++ src/pages/Community/PostDetail.tsx
@@ -1,184 +1,69 @@
-import { DotsThreeVertical } from '@phosphor-icons/react';
-import { useState, useEffect } from 'react';
-// import { useParams } from 'react-router-dom';
+import { DownloadSimple } from '@phosphor-icons/react';
+import { Navigate, useParams } from 'react-router-dom';
 
-import { get, patch, del } from '@/libs/api';
-import { comments } from '@/mock/Comment';
-// import { posts } from '@/mock/Community';
+import useModal from '@/hooks/useModal';
+import { mockPost } from '@/mock/Community';
+import useBoundStore from '@/stores';
 
-import { PostDetails } from './components/PostData';
-import { Comment } from './types/Comment';
+import LevelUpButton from './components/LevelUpButton';
+import PostActions from './components/PostActions';
+import PostFormModal from './components/PostFormModal';
+import { boardMetaData } from './constants/communityInfo';
 
-function PostDetail() {
-  // const { postId } = useParams<{ postId: string }>();
-  // const numericPostId = parseInt(postId || '0', 10);
-  // const post = posts.find(p => p.id === numericPostId);
-  const [commentText, setCommentText] = useState<string>('');
-  const [commentList, setCommentList] = useState<Comment[]>([]);
-  const [menuVisible, setMenuVisible] = useState<{ [key: number]: boolean }>(
-    {},
-  );
-  // const [isEditing, setIsEditing] = useState(false);
-  const [isEditing, setIsEditing] = useState<number | null>(null); // State to track editing comment ID
-  const [editContent, setEditContent] = useState(comments || '');
+const data = mockPost;
 
-  const toggleMenu = (commentId: number) => {
-    setMenuVisible(prev => ({
-      ...prev,
-      [commentId]: !prev[commentId],
-    }));
-  };
+export default function PostDetails() {
+  const { isVisible: isEditModalVisible, toggleModal: toggleEditModal } =
+    useModal();
+  const { category } = useParams();
+  const user = useBoundStore(state => state.user);
 
-  // 댓글 로드
-  useEffect(() => {
-    get<Comment[]>(`/posts/${post?.id}/comments/`)
-      .then(res => {
-        setCommentList(res);
-        console.log('댓글 받기 성공');
-      })
-      .catch(err => {
-        console.log('error:', err);
-      });
-  }, []);
+  if (!category || !(category in boardMetaData))
+    return <Navigate to='/community' replace />;
 
-  if (!post) {
-    return <div>게시글을 찾을 수 없습니다.</div>;
+  if (!data) {
+    alert('게시글 정보를 찾을 수 없습니다.');
+    return <Navigate to={`/community/${category}`} replace />;
   }
 
-  // 댓글 작성(추가)
-  const handleSubmitComment = async () => {
-    const requestData = {
-      // id: 0,
-      // author: '사용자 이름',
-      content: commentText,
-      // timestamp: new Date().toISOString(),
-    };
-    try {
-      const newComment: Comment = await post<Comment[]>(
-        `posts/${post?.id}/comments/`,
-        requestData,
-      );
-      console.log('댓글 작성 성공!', newComment);
-      setCommentList(prev => [...prev, newComment]);
-      setCommentText('');
-    } catch (err) {
-      console.log('댓글 작성 실패..', err);
-    }
-  };
-
-  const handleEditConfirm = async () => {
-    try {
-      await patch(`/comments/${isEditing}`, { content: editContent });
-      setCommentList(prev =>
-        prev.map(comment =>
-          comment.id === isEditing
-            ? { ...comment, content: editContent }
-            : comment,
-        ),
-      );
-      setIsEditing(null);
-      alert('수정이 완료되었습니다.');
-    } catch (error) {
-      alert('수정 중 오류가 발생했습니다.');
-    }
-  };
-
-  const handleDelete = async (commentId: number) => {
-    const confirmed = window.confirm('해당 댓글을 정말 삭제하시겠습니까?');
-    if (confirmed && commentId) {
-      try {
-        await del(`posts/${post?.id}/comments/${commentId}`);
-        alert('삭제 성공');
-        window.location.reload();
-      } catch (error) {
-        alert('삭제 중 오류가 발생했습니다.');
-      }
-    }
-  };
+  // 등업 게시판 & 관리자 권한일 경우에만 등업시키기 버튼 렌더링
+  const isLevelUpButtonVisible =
+    user && user.role === '관리자' && category === 'levelup';
 
   return (
-    <div>
-      <PostDetails post={post} onClose={() => {}} />
-      {/* 해당 글에 대한 댓글 보여주기 */}
-      <div className='px-20 mb-20'>
-        <p className='mt-20 text-sm mb-[10px]'>댓글 {post.comments}</p>
-
-        {/* 댓글 목록 렌더링 */}
-        <div className='mt-4'>
-          {commentList?.map((comment: Comment) => (
-            <div key={comment.id} className='border-b py-6'>
-              <div className='flex items-center justify-between'>
-                {/* <div className='flex items-center '>
-                  <p className='font-semibold mr-4'>{comment.author}</p>
-                  <p className='text-xs text-gray-500'>{comment.timestamp}</p>
-                </div> */}
-                <button
-                  onClick={() => toggleMenu(comment.id)}
-                  className='relative'
-                >
-                  <DotsThreeVertical size={15} className='cursor-pointer' />
-                  {menuVisible && (
-                    <div className='absolute right-0 mt-2 w-48 bg-white shadow-lg z-10 rounded-lg border'>
-                      <button
-                        onClick={() => setIsEditing(true)}
-                        className='block w-full py-2 text-sm text-gray-700 hover:bg-gray-200'
-                      >
-                        수정
-                      </button>
-                      <hr />
-                      <button
-                        onClick={handleDelete}
-                        className='block w-full px-4 py-2 text-sm text-red-600 hover:bg-gray-200'
-                      >
-                        삭제
-                      </button>
-                    </div>
-                  )}
-                </button>
-              </div>
-              {/* <p>{comment.content}</p> */}
-              {isEditing ? (
-                <>
-                  <textarea
-                    value={editContent}
-                    onChange={e => setEditContent(e.target.value)}
-                    rows={10}
-                  />
-                  <div className='flex justify-end mt-4'>
-                    <button
-                      onClick={handleEditConfirm}
-                      className='mb-7 px-8 py-6 bg-primary-300 text-white rounded-lg hover:bg-pimary-200'
-                    >
-                      수정완료
-                    </button>
-                  </div>
-                </>
-              ) : (
-                // Show post content if not in editing mode
-                <p className='text-sm'>{comment.content}</p>
-              )}
-            </div>
-          ))}
+    <div className='text-left mt-10'>
+      <hr className='border-1 border-black w-full' />
+      <div className='flex flex-col sm:flex-row justify-between px-20 py-4 sm:py-10 bg-gray-100 relative'>
+        <h2 className='text-20 font-bold'>{data.title}</h2>
+      </div>
+      <hr className='border-1 border-gray-300 w-full' />
+      <div className='flex justify-between px-10 md:px-20 py-10 flex-wrap gap-y-6'>
+        <div className='flex-row-center space-x-16 shrink-0'>
+          <p className=' text-primary-400 relative text-sm pipe-after'>
+            {data.writer}
+          </p>
+          <p className='text-gray-400 relative text-sm pipe-after'>{`${data.createdAt.split('T')[0]}`}</p>
+          <p className='text-gray-400 text-sm'>{`조회 ${data.viewCount}`}</p>
         </div>
-
-        {/* 댓글 입력 폼 */}
-        <div className='flex flex-row space-x-8 mt-20'>
-          <textarea
-            placeholder='자신의 의견을 자유롭게 표현해주세요.'
-            value={commentText}
-            onChange={e => setCommentText(e.target.value)}
-            className='w-full border rounded p-2 resize-none'
-          />
-          <button
-            onClick={handleSubmitComment}
-            className='w-1/6 mt-2 py-2 px-4 bg-gray-300 rounded hover:bg-primary-400'
-          >
-            등록
+        <div className='flex items-center gap-12 ml-auto'>
+          <button>
+            <p className='flex-row-center gap-4 text-sm text-gray-500'>
+              파일 다운받기
+              <DownloadSimple className='mt-1 block' size={17} />
+            </p>
           </button>
+          <PostActions id={data.postId} onEdit={toggleEditModal} />
         </div>
       </div>
+      <hr className='border-1 border-gray-300 w-full' />
+      <p className='px-20 my-100'>{data.content}</p>
+      <hr className='border-1 border-gray-300 w-full' />
+      {isLevelUpButtonVisible && <LevelUpButton />}
+      <PostFormModal
+        post={data}
+        isVisible={isEditModalVisible}
+        onClose={toggleEditModal}
+      />
     </div>
   );
 }
-
-export default PostDetail;
