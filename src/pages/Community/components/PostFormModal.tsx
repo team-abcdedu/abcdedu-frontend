f6a972397d9928c2e4df84b1d9b35bf02f64bb89
mihee <s2809h29@gmail.com>
2024-09-17 21:59:45 +0900

    refactor: 게시글 작성 모달 리팩토링 #113
    - react-hook-form 적용
    - 파일 크기 제한 (20MB) 추가

---
+++ src/pages/Community/components/PostFormModal.tsx
@@ -0,0 +1,99 @@
+import { X } from '@phosphor-icons/react';
+
+import Modal from '@/components/Modal';
+import { Post } from '@/types/Community';
+
+import usePostForm from '../hooks/usePostForm';
+
+export interface WritePostModalProps {
+  post?: Post;
+  isVisible: boolean;
+  onClose: () => void;
+}
+
+export default function PostFormModal({
+  post,
+  isVisible,
+  onClose,
+}: WritePostModalProps) {
+  const { register, reset, errors, handleFileChange, onSubmit } = usePostForm({
+    post,
+    onSuccess: onClose,
+  });
+
+  const handleClose = () => {
+    onClose();
+    reset();
+  };
+
+  return (
+    <Modal isVisible={isVisible} onClose={handleClose} size='lg'>
+      <Modal.Content>
+        <form className='flex flex-col' onSubmit={onSubmit}>
+          <div className='flex justify-between items-center'>
+            <h2 className='text-lg font-bold'>글쓰기</h2>
+            <button type='button' onClick={handleClose}>
+              <X size={24} />
+            </button>
+          </div>
+          <div className='p-4 [&_span]:text-14 [&_span]:text-red-500'>
+            <div className='my-8'>
+              <input
+                id='title'
+                {...register('title', { required: true })}
+                type='text'
+                placeholder='제목을 입력하세요.'
+                className='w-full border rounded py-6 px-8'
+              />
+              {errors.title && <span>제목을 입력하세요.</span>}
+            </div>
+            <div className='mb-20'>
+              <textarea
+                id='content'
+                {...register('content', { required: true })}
+                placeholder='내용을 입력하세요.'
+                className='w-full h-64 border rounded py-6 px-8 min-h-[384px]'
+              />
+              {errors.content && (
+                <span className='block -mt-4'>내용을 입력하세요.</span>
+              )}
+            </div>
+            <div className='mb-20 flex items-center space-x-14'>
+              <label htmlFor='file' className='block text-gray-700 text mb-2'>
+                첨부파일
+              </label>
+              <input
+                id='file'
+                type='file'
+                className='w-2/3 border rounded p-2 h-full'
+                onChange={handleFileChange}
+              />
+            </div>
+            <div className='flex mb-4 items-center gap-4'>
+              <input type='checkbox' id='secret' {...register('secret')} />
+              <label htmlFor='secret' className='text-14'>
+                비밀글 설정
+              </label>
+            </div>
+            <div className='flex items-center mb-20 gap-4'>
+              <input
+                type='checkbox'
+                id='commentAllow'
+                {...register('commentAllow')}
+              />
+              <label htmlFor='commentAllow' className='text-14'>
+                댓글 허용
+              </label>
+            </div>
+            <button
+              className='block w-160 py-8 px-16 rounded-2xl bg-primary-400 text-white mx-auto'
+              type='submit'
+            >
+              확인
+            </button>
+          </div>
+        </form>
+      </Modal.Content>
+    </Modal>
+  );
+}
