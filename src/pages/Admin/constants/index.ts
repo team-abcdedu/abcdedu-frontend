7e6993b6f4d8055c52a535bc8525b8663c34301d
PHJoon <phj9503@gmail.com>
2024-09-17 09:35:22 +0900

    fix: 타입 오류 수정

--- src/pages/Admin/constants/index.ts
+++ src/pages/Admin/constants/index.ts
@@ -3,7 +3,7 @@ import { ContactTypeLabel } from '@/types/contact';
 import { TableColumnMap, TableColumns } from '../types';
 
 export const tableColumns: TableColumns = {
-  class: ['title', 'type', 'description', 'subClasses'],
+  class: ['title', 'subTitle', 'description', 'subClasses'],
   assignment: ['id', 'createdAt', 'name'],
   survey: ['id', 'createdAt', 'class', 'name'],
   theory: ['id', 'class', 'file'],
@@ -14,7 +14,7 @@ export const tableColumns: TableColumns = {
 export const tableColumnMap: TableColumnMap = {
   class: {
     title: '클래스명',
-    type: '타입',
+    subTitle: '부제목',
     description: '설명',
     subClasses: '서브클래스',
   },
