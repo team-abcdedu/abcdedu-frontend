947403d308a297cb70935551492a02ab830fa8a4
PHJoon <phj9503@gmail.com>
2024-09-19 17:44:45 +0900

    fix: 시험 파일 버튼 수정 #123

--- src/pages/Classes/components/ExamContent.tsx
+++ src/pages/Classes/components/ExamContent.tsx
@@ -8,35 +8,33 @@ interface ExamContentProps {
 function ExamContent({ pdfUrl, pdfFileId }: ExamContentProps) {
   const { data: hwp } = useGetSubClassStudentFile({
     assignmentAnswerFileId: pdfFileId,
+    enabled: !!pdfFileId,
   });
 
   return (
-    <div className={'w-full'}>
+    <div className={'w-full pb-100'}>
       <div className={'w-full flex-col-center sm:flex-row-center gap-30'}>
         <a
           href={pdfUrl}
           target={'_blank'}
+          rel={'noreferrer noopener'}
           className={
             'p-10 text-20 border-2 border-primary-300 rounded-lg hover:bg-primary-300 hover:text-white transition ease-in-out delay-50'
           }
-          rel='noreferrer'
         >
-          pdf 새 탭에서 열기
+          가이드 pdf 다운로드
         </a>
         <a
           href={hwp?.filePresignedUrl}
+          target={'_blank'}
+          rel={'noreferrer noopener'}
           className={
             'p-10 text-20 border-2 border-primary-300 rounded-lg hover:bg-primary-300 hover:text-white transition ease-in-out delay-50'
           }
         >
-          과제 제출용 hwp 다운로드
+          제출용 hwp 다운로드
         </a>
       </div>
-      <iframe
-        src={pdfUrl}
-        title={'exam-pdf'}
-        className={'w-full h-[600px] md:h-[1000px] p-40 md:p-80'}
-      ></iframe>
     </div>
   );
 }
