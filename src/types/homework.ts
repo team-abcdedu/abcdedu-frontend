7112245fd4a0a10948ea451a5836de3f805f793e
PHJoon <phj9503@gmail.com>
2024-09-19 15:08:18 +0900

    feat: 과제 타입 수정  #123

--- src/types/homework.ts
+++ src/types/homework.ts
@@ -1,51 +1,20 @@
-export interface QuestionOption {
-  index: number;
-  content: string;
-}
-
-export type QuestionType =
-  | 'SUBJECTIVE'
-  | 'SINGLE_OPTION'
-  | 'MULTIPLE_OPTION'
-  | 'SHORT_ANSWER';
-
-export interface QuestionInfo {
-  index: number;
-  type: QuestionType;
-  title: string;
-  description: string;
-  score: number;
-  options?: QuestionOption[];
-}
-
 export interface HomeworkSummary {
   id: number;
   title: string;
   subTitle: string;
 }
 
+export interface HomeworkQuestion {
+  orderNumber: string;
+  content: string;
+  isAnswerRequired: boolean;
+}
+
 export interface HomeworkInfo extends HomeworkSummary {
   description: string;
-  questions: QuestionInfo[];
-  additionalDescription: string;
+  questionGetResponses: HomeworkQuestion[];
 }
 
-export interface MyHomeworkAnswerInfo {
-  questionId: number;
-  content?: string;
-  optionIndex?: number;
-  optionIndexes?: number[];
-}
-
-export type CreateHomeworkData = Omit<HomeworkInfo, 'id'>;
-
-export type CreateQuestionOption = QuestionOption & { isAnswer: boolean };
-
-export interface CreateQuestion {
-  index: number;
-  type: QuestionType;
-  title: string;
-  description: string;
-  score: number;
-  options?: CreateQuestionOption[];
+export interface HomeworkAnswer {
+  answer: string;
 }
