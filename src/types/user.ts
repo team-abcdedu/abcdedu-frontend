99028110ba3cc947ea07699dc97fe85ca4709df8
mihee <s2809h29@gmail.com>
2024-09-21 00:41:34 +0900

    fix: User type 수정 #135

--- src/types/user.ts
+++ src/types/user.ts
@@ -1,10 +1,10 @@
 export interface User {
   name: string;
-  role: string; // 더 strict하게 수정 가능성 있음
+  role: string;
+  email: string;
 }
 
 export interface UserInfo extends User {
-  email: string;
   school: string | null;
   studentId: number | null;
   imageUrl: string | null;
