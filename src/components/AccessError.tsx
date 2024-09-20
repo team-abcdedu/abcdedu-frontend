05ce7751db9a8e4187d5bfe69000144d91e8bebc
PHJoon <phj9503@gmail.com>
2024-09-20 12:56:21 +0900

    feat: AccessError 문구 수정 #112

--- src/components/AccessError.tsx
+++ src/components/AccessError.tsx
@@ -4,22 +4,25 @@ function AccessError({
   type,
   errorCode,
   linkUrl,
+  linkString,
 }: {
   type: '과제' | '설문';
   errorCode: number;
   linkUrl: string;
+  linkString: string;
 }) {
   return (
     <div className='flex-col-center h-[500px] gap-10 py-120'>
       <p className='text-center'>
-        {errorCode === 401 && `로그인 후 ${type}를 열람할 수 있습니다.`}
-        {errorCode === 403 && `학생 등급 이상만 ${type}를 열람할 수 있습니다.`}
+        {errorCode === 401 && `로그인 후 ${type}을(를) 열람할 수 있습니다.`}
+        {errorCode === 403 &&
+          `학생 등급 이상만 ${type}을(를) 열람할 수 있습니다.`}
       </p>
       <Link
         to={linkUrl}
         className='px-12 py-6 bg-primary-400 rounded-[20px] text-14 text-white'
       >
-        홈으로
+        {linkString}
       </Link>
     </div>
   );
