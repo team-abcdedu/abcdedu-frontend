96c70f85dbb22516ebb7fd057694dac9964c9973
mihee <s2809h29@gmail.com>
2024-09-24 17:12:26 +0900

    fix: ApiError status number 타입으로 수정 #179

--- src/libs/errors.ts
+++ src/libs/errors.ts
@@ -1,10 +1,10 @@
 /** @description 400번대 서버 api 에러 */
 export class ApiError extends Error {
-  status: number | undefined;
+  status: number;
 
   errorCode: string | undefined;
 
-  constructor(message: string, status?: number, errorCode?: string) {
+  constructor(message: string, status: number, errorCode?: string) {
     super(message);
     this.name = 'Api Error';
     this.status = status;
