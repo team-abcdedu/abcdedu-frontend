39d2fc13ce9d04766a77c1afdb6b92ef11750804
hyungjpa <100325940+PHJoon@users.noreply.github.com>
2024-09-24 15:57:15 +0900

    feat: 시험 pdf 스크롤 시 헤더 display 추가, 임시 pdf url 추가 #171

--- src/pages/Classes/components/ExamContent.tsx
+++ src/pages/Classes/components/ExamContent.tsx
@@ -1,39 +1,77 @@
+import { useCallback, useEffect, useRef } from 'react';
+
 import useGetSubClassStudentFile from '@/hooks/class/useGetSubClassStudentFile';
+import useHeaderStore from '@/stores/headerStore';
 
 interface ExamContentProps {
   examFileUrl: string | undefined;
   studentFileId: number | undefined;
 }
 
+const url = 'https://karf.or.kr/wp-content/uploads/2022/03/Download_Sample.pdf';
+
 function ExamContent({ examFileUrl, studentFileId }: ExamContentProps) {
+  const buttonStyle =
+    'p-5 md:p-10 text-18 md:text-20 border-2 border-primary-300 rounded-lg hover:bg-primary-300 hover:text-white transition ease-in-out delay-50';
+
   const { data: examStudentFile } = useGetSubClassStudentFile({
     assignmentAnswerFileId: studentFileId,
     enabled: !!studentFileId,
   });
 
+  const iframeRef = useRef<HTMLIFrameElement | null>(null);
+
+  const { headerRef } = useHeaderStore();
+  const handleIntersect = useCallback(
+    (entries: IntersectionObserverEntry[]) => {
+      entries.forEach(entry => {
+        if (entry.isIntersecting) {
+          headerRef?.classList.add('hidden');
+        } else {
+          headerRef?.classList.remove('hidden');
+        }
+      });
+    },
+    [headerRef],
+  );
+
+  useEffect(() => {
+    if (iframeRef.current && headerRef) {
+      const observer = new IntersectionObserver(handleIntersect, {
+        root: null,
+        rootMargin: '0px',
+        threshold: 0.6,
+      });
+      observer.observe(iframeRef.current);
+    }
+  }, [handleIntersect, headerRef]);
+
   return (
-    <div className={'w-full pb-100'}>
+    <div className={'w-full pb-50 flex-col-center gap-30'}>
       <div className={'w-full flex-col-center sm:flex-row-center gap-30'}>
-        <a
-          href={examFileUrl}
-          download
-          className={
-            'p-10 text-20 border-2 border-primary-300 rounded-lg hover:bg-primary-300 hover:text-white transition ease-in-out delay-50'
-          }
-        >
+        <a href={examFileUrl} download className={buttonStyle}>
           시험 파일 다운로드
         </a>
         {examStudentFile && (
           <a
             href={examStudentFile.filePresignedUrl}
             download
-            className={
-              'p-10 text-20 border-2 border-primary-300 rounded-lg hover:bg-primary-300 hover:text-white transition ease-in-out delay-50'
-            }
+            className={buttonStyle}
           >
             제출용 파일 다운로드
           </a>
         )}
+      </div>
+      <div
+        className={'min-w-[300px] w-[85%] h-[500px] sm:h-[700px] md:h-[900px]'}
+      >
+        <iframe
+          ref={iframeRef}
+          // src={examFileUrl}
+          src={url}
+          title={'exam'}
+          className={'w-full h-full'}
+        />
       </div>
     </div>
   );
