6e74e429866ed8884e3cbfd71d7e57d1560f485c
hyungjpa <100325940+PHJoon@users.noreply.github.com>
2024-09-25 16:50:43 +0900

    feat: ExamContent pdf 새 탭에서 열기 및 해당 동작 버튼 추가 #194

--- src/pages/Classes/components/ExamContent.tsx
+++ src/pages/Classes/components/ExamContent.tsx
@@ -1,9 +1,8 @@
-import { useCallback, useEffect, useRef, useState } from 'react';
+import { useCallback } from 'react';
 
 import useGetSubClassGeneralFile from '@/hooks/class/useGetSubClassGeneralFile';
 import useGetSubClassStudentFile from '@/hooks/class/useGetSubClassStudentFile';
 import useGetPdfUrl from '@/pages/Classes/hooks/useGetPdfUrl';
-import useBoundStore from '@/stores';
 import { getFileName } from '@/utils/getFileName';
 
 interface ExamContentProps {
@@ -14,8 +13,6 @@ function ExamContent({ assignmentFileId }: ExamContentProps) {
   const buttonStyle =
     'p-5 md:p-10 text-18 md:text-20 border-2 border-primary-300 rounded-lg hover:bg-primary-300 hover:text-white transition ease-in-out delay-50';
 
-  const [isPdf, setIsPdf] = useState(false);
-
   const { data: examFile } = useGetSubClassGeneralFile({
     assignmentFileId,
   });
@@ -25,50 +22,42 @@ function ExamContent({ assignmentFileId }: ExamContentProps) {
     enabled: !!examFile?.assignmentAnswerFileId,
   });
 
-  const { pdfUrl } = useGetPdfUrl({ s3Url: examFile?.filePresignedUrl });
+  const getFileExtension = useCallback((fileName: string | undefined) => {
+    return fileName?.split('.').pop();
+  }, []);
 
-  const iframeRef = useRef<HTMLIFrameElement | null>(null);
+  const { pdfUrl } = useGetPdfUrl({
+    s3Url: examFile?.filePresignedUrl,
+    enabled:
+      examFile &&
+      getFileExtension(getFileName(examFile?.filePresignedUrl)) === 'pdf',
+  });
 
-  const { headerRef } = useBoundStore();
-  const handleIntersect = useCallback(
-    (entries: IntersectionObserverEntry[]) => {
-      entries.forEach(entry => {
-        if (entry.isIntersecting) {
-          headerRef?.classList.add('hidden');
-        } else {
-          headerRef?.classList.remove('hidden');
-        }
-      });
-    },
-    [headerRef],
-  );
-
-  useEffect(() => {
-    if (iframeRef.current && headerRef) {
-      const observer = new IntersectionObserver(handleIntersect, {
-        root: null,
-        rootMargin: '0px',
-        threshold: 0.6,
-      });
-      observer.observe(iframeRef.current);
+  const handleFileOpen = () => {
+    const newWindow = window.open(pdfUrl, '_blank', 'noopener,noreferrer');
+    if (newWindow) {
+      newWindow.opener = null;
     }
-  }, [handleIntersect, headerRef]);
+  };
 
-  useEffect(() => {
-    if (
-      examFile?.filePresignedUrl &&
-      getFileName(examFile?.filePresignedUrl)?.split('.').pop() === 'pdf'
-    ) {
-      setIsPdf(true);
-    }
-  }, [examFile]);
+  if (!examFile) {
+    return null;
+  }
 
   return (
     <div className={'w-full pb-50 flex-col-center gap-30'}>
       <div className={'w-full flex-col-center sm:flex-row-center gap-30'}>
-        {!isPdf && (
+        {getFileExtension(getFileName(examFile?.filePresignedUrl)) === 'pdf' ? (
+          <button
+            type={'button'}
+            className={buttonStyle}
+            onClick={handleFileOpen}
+          >
+            새 탭에서 열기 ({getFileName(examFile?.filePresignedUrl)})
+          </button>
+        ) : (
           <a href={examFile?.filePresignedUrl} download className={buttonStyle}>
-            시험 파일 다운로드
+            파일 다운로드 ({getFileName(examFile?.filePresignedUrl)})
           </a>
         )}
 
@@ -78,25 +67,10 @@ function ExamContent({ assignmentFileId }: ExamContentProps) {
             download
             className={buttonStyle}
           >
-            제출용 파일 다운로드
+            파일 다운로드 ({getFileName(examStudentFile.filePresignedUrl)})
           </a>
         )}
       </div>
-
-      {isPdf && (
-        <div
-          className={
-            'min-w-[300px] w-[85%] h-[500px] sm:h-[700px] md:h-[900px]'
-          }
-        >
-          <iframe
-            ref={iframeRef}
-            src={pdfUrl}
-            title={'exam'}
-            className={'w-full h-full'}
-          />
-        </div>
-      )}
     </div>
   );
 }
