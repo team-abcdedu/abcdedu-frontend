2fc981dd6e15bdb150a7e487ad8cb44f2e3cec51
hyungjpa <100325940+PHJoon@users.noreply.github.com>
2024-09-27 17:25:49 +0900

    feat: MobileSidebar 구분선 추가 #200

--- src/components/Header/MobileSidebar.tsx
+++ src/components/Header/MobileSidebar.tsx
@@ -10,9 +10,10 @@ function MobileSidebar() {
     <div
       id={'mobile-sidebar'}
       className={
-        'absolute w-[200px] h-dvh top-[-25.76px] right-[-220px] pt-60 pb-30 flex flex-col gap-20 bg-white z-modal'
+        'absolute w-[200px] h-dvh top-[-25.76px] right-[-220px] pt-60 pb-30 flex flex-col-center bg-white z-modal'
       }
     >
+      <div className={'w-1/2 h-1 bg-neutral-100 mt-10'}></div>
       <nav className={'w-full h-[80%] relative overflow-hidden'}>
         <ul
           className={
@@ -25,7 +26,10 @@ function MobileSidebar() {
           {user?.role === '관리자' && <MobileNav item={adminNavItem} />}
         </ul>
       </nav>
-      <HeaderButtons />
+      <div className={'w-1/2 h-1 bg-neutral-100 mb-10'}></div>
+      <div className={'h-[15%] flex-row-center'}>
+        <HeaderButtons />
+      </div>
     </div>
   );
 }
