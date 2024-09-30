b48fc36141a845a5dbc87147c0ed1ecf7136d95d
hyungjpa <100325940+PHJoon@users.noreply.github.com>
2024-09-30 14:51:07 +0900

    refactor: accessToken store에서 제거 #207

--- src/stores/authSlice.ts
+++ src/stores/authSlice.ts
@@ -1,12 +1,10 @@
 import { StateCreator } from 'zustand';
 
 type State = {
-  accessToken: string | null;
   isAutoLogin: boolean;
 };
 
 type Actions = {
-  fetchAccessToken: (accessToken: string | null) => void;
   resetAuthState: () => void;
   setIsAutoLogin: () => void;
 };
@@ -14,13 +12,11 @@ type Actions = {
 export type AuthSlice = State & Actions;
 
 const initialState: State = {
-  accessToken: null,
   isAutoLogin: false,
 };
 
 export const createAuthSlice: StateCreator<AuthSlice> = set => ({
   ...initialState,
-  fetchAccessToken: accessToken => set({ accessToken }),
   resetAuthState: () => set(initialState),
   setIsAutoLogin: () => set({ isAutoLogin: true }),
 });
