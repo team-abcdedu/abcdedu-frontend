99a63f11466fe5f36c9bcdfc0db2e995d99f789d
mihee <s2809h29@gmail.com>
2024-10-02 13:57:47 +0900

    refactor: rollup manualChunks를 이용한 번들 최적화 #212
    - node_modules 공통 모듈 분리

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,18 @@ export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, process.cwd());
   return {
     plugins: [react(), svgr()],
+    build: {
+      rollupOptions: {
+        output: {
+          manualChunks: id => {
+            if (id.indexOf('node_modules') !== -1) {
+              const module = id.split('node_modules/').pop().split('/')[0];
+              return `vendor-${module}`;
+            }
+          },
+        },
+      },
+    },
     server: {
       host: '0.0.0.0',
       port: 3000,
