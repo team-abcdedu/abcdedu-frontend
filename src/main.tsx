6483c1f2408c3168b7e037c5e28843dadf681f03
mihee <s2809h29@gmail.com>
2024-10-08 17:20:24 +0900

    feat: vite preloadError 예외 처리 #191

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,13 @@ import { createRoot } from 'react-dom/client';
 
 import App from './App';
 
+// 배포 버전이 변경될 때, 이전 버전 사용자에게 발생할 수 있는 import error 처리
+// https://ko.vitejs.dev/guide/build#load-error-handling
+window.addEventListener('vite:preloadError', event => {
+  console.error('Preload error occurred:', event.payload);
+  window.location.reload();
+});
+
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
