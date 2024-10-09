5454269ba3c16fbce49b809346fea16d6c45e140
mihee <s2809h29@gmail.com>
2024-10-09 22:06:47 +0900

    feat: sentry 환경 구축 #191

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,9 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 
 import App from './App';
+import { initSentry } from './libs/sentry';
+
+initSentry();
 
 // 배포 버전이 변경될 때, 이전 버전 사용자에게 발생할 수 있는 import error 처리
 // https://ko.vitejs.dev/guide/build#load-error-handling
--- vite.config.ts
+++ vite.config.ts
@@ -1,3 +1,4 @@
+import { sentryVitePlugin } from '@sentry/vite-plugin';
 import react from '@vitejs/plugin-react-swc';
 import { defineConfig, loadEnv } from 'vite';
 import svgr from 'vite-plugin-svgr';
@@ -5,11 +6,27 @@ import svgr from 'vite-plugin-svgr';
 export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, process.cwd());
   return {
-    plugins: [react(), svgr()],
+    plugins: [
+      react(),
+      svgr(),
+      sentryVitePlugin({
+        org: 'abcdedu',
+        project: 'abcdedu-frontend',
+        authToken: env.SENTRY_AUTH_TOKEN,
+        // 생성된 sourcemap sentry에 업로드 후 삭제
+        sourcemaps: {
+          filesToDeleteAfterUpload: '**/*.map',
+        },
+      }),
+    ],
     build: {
+      sourcemap: mode === 'production', // Production에서만 sourcemap 생성
       rollupOptions: {
         output: {
           manualChunks: id => {
+            if (id.includes('@sentry')) {
+              return '@sentry';
+            }
             if (id.indexOf('node_modules') !== -1) {
               const module = id.split('node_modules/').pop().split('/')[0];
               return `vendor-${module}`;
