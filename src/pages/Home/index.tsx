7527bbb5781ab55e05078221579b79b12821f7f8
mihee <s2809h29@gmail.com>
2024-10-15 23:04:43 +0900

    fix: 로깅 테스트용 코드 제거 #191

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -7,13 +7,8 @@ import Hero from './components/Hero';
 import Message from './components/Message';
 
 export default function Home() {
-  // sentry 로깅 테스트 후 삭제 예정
-  const onClick = () => {
-    throw new Error('This is a test error!');
-  };
   return (
     <div>
-      <button onClick={onClick}>오류 테스트용</button>
       <Head />
       <Hero />
       <AboutUs />
