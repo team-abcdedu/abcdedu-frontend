8577aa0f9585e5a57f6742607a7e9059f1fe04a5
mihee <s2809h29@gmail.com>
2024-10-24 21:56:54 +0900

    feat: Hero 버튼 추가 #229

--- src/pages/Home/components/Hero.tsx
+++ src/pages/Home/components/Hero.tsx
@@ -1,3 +1,6 @@
+import { CaretDown } from '@phosphor-icons/react';
+import { motion } from 'framer-motion';
+
 import HeroImage from '@/assets/images/hero-img.svg?react';
 
 import Reviews from './Reviews';
@@ -9,7 +12,7 @@ export default function Hero() {
       className='bg-white min-h-[813px] flex flex-col leading-[1.4] 
       hero-gradient pt-135 lg:pb-70 pb-72'
     >
-      <div className='lg:mb-24 mb-100'>
+      <div className='flex-col-center lg:mb-24 mb-100 relative'>
         <h1 className='text-center text-primary-400 leading-[1.4]'>
           <span
             className='block lg:text-60 sm:text-48 text-32 font-bold 
@@ -22,6 +25,20 @@ export default function Hero() {
             데이터 <br className='sm:hidden' /> 사이언스
           </span>
         </h1>
+        <motion.div
+          className='absolute -bottom-120 w-100 h-38 flex-row-center bg-white rounded-full text-primary-400'
+          style={{ boxShadow: '0px 3px 8px 0px #0000000A' }}
+          animate={{
+            y: [0, -5, 0],
+          }}
+          transition={{
+            duration: 0.8,
+            repeat: Infinity,
+            ease: 'easeInOut',
+          }}
+        >
+          <CaretDown />
+        </motion.div>
       </div>
       <div className='w-full max-w-[1215px] mx-auto'>
         <HeroImage className='hidden lg:block w-180 h-196 ml-32' />
