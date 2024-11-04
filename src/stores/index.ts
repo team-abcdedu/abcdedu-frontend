2dda76cd97f0aa3b93ebd7d3e8e0fbddc4134345
mihee <s2809h29@gmail.com>
2024-11-04 17:22:19 +0900

    feat: 이메일 인증 상태 관리를 위한 emailVerificationSlice 추가 #237

--- src/stores/index.ts
+++ src/stores/index.ts
@@ -2,15 +2,20 @@ import { create } from 'zustand';
 import { persist } from 'zustand/middleware';
 
 import { AuthSlice, createAuthSlice } from './authSlice';
+import {
+  EmailVerificationSlice,
+  createEmailVerificationSlice,
+} from './emailVerificationSlice';
 import { createHeaderSlice, HeaderSlice } from './headerSlice';
 import { UserSlice, createUserSlice } from './userSlice';
 
-type StoreState = AuthSlice & UserSlice & HeaderSlice;
+type StoreState = AuthSlice & EmailVerificationSlice & UserSlice & HeaderSlice;
 
 const useBoundStore = create<StoreState>()(
   persist(
     (set, get, api) => ({
       ...createAuthSlice(set, get, api),
+      ...createEmailVerificationSlice(set, get, api),
       ...createUserSlice(set, get, api),
       ...createHeaderSlice(set, get, api),
     }),
