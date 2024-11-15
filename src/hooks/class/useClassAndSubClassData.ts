82e2a29070837eb214a7c8e88c37eb8a5458805a
hyungjpa <100325940+PHJoon@users.noreply.github.com>
2024-11-15 18:32:22 +0900

    feat: useClassAndSubClassData 추가 #260

---
+++ src/hooks/class/useClassAndSubClassData.ts
@@ -0,0 +1,25 @@
+import { useMemo } from 'react';
+import { useParams } from 'react-router-dom';
+
+import useBoundStore from '@/stores';
+
+// 현재 경로에 해당하는 클래스, 서브클래스 데이터
+function useClassAndSubClassData() {
+  const { classId, subClassId } = useParams();
+  const classDataList = useBoundStore(state => state.classDataList);
+
+  const classData = useMemo(() => {
+    return classDataList?.find(d => d.title === classId?.toUpperCase());
+  }, [classDataList, classId]);
+  const subClassData = useMemo(() => {
+    return classData?.subClasses.find(
+      d => d.orderNumber === Number(subClassId),
+    );
+  }, [classData, subClassId]);
+
+  const isSubClassPage = classId && subClassId;
+
+  return { classData, subClassData, isSubClassPage };
+}
+
+export default useClassAndSubClassData;
