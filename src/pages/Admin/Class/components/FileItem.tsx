eb37c283c2f6bec2b998d070b49297cdd0627eb0
hyungjpa <100325940+PHJoon@users.noreply.github.com>
2024-11-19 20:54:42 +0900

    refactor: FileItem 에서 FileUpdateModal 분리 #260

--- src/pages/Admin/Class/components/FileItem.tsx
+++ src/pages/Admin/Class/components/FileItem.tsx
@@ -1,12 +1,6 @@
-import { useState } from 'react';
-import { SubmitHandler } from 'react-hook-form';
-
-import Loader from '@/components/Loader';
 import useSubClassFile from '@/hooks/class/useSubClassFile';
-import useFileUpdateForm, {
-  IFileUpdateForm,
-} from '@/pages/Admin/Class/hooks/useFileUpdateForm';
-import useFileUpdateMutation from '@/pages/Admin/Class/hooks/useFileUpdateMutation';
+import useModal from '@/hooks/useModal';
+import FileUpdateModal from '@/pages/Admin/Class/components/FileUpdateModal';
 import { FileInfo } from '@/types/class';
 
 function FileItem({
@@ -31,38 +25,11 @@ function FileItem({
     return 'bg-yellow-50';
   };
 
-  const [updateFileInputOpen, setUpdateFileInputOpen] = useState(false);
-
   const { fileData } = useSubClassFile({
     fileId,
   });
 
-  const { register, errors, handleSubmit, fieldRules, reset } =
-    useFileUpdateForm();
-  const { mutation } = useFileUpdateMutation({ subLectureId, fileId });
-
-  const handleOpenUpdateFileInput = () => {
-    setUpdateFileInputOpen(prev => !prev);
-    if (updateFileInputOpen) reset();
-  };
-
-  const onSubmit: SubmitHandler<IFileUpdateForm> = data => {
-    const result = window.confirm('파일을 수정하시겠습니까?');
-    if (result) {
-      mutation.mutate(
-        { fileId, file: data.file[0] },
-        {
-          onSuccess: () => {
-            alert('파일이 수정됐습니다.');
-            reset();
-          },
-          onError: error => {
-            alert(error.message || '파일 수정에 실패했습니다.');
-          },
-        },
-      );
-    }
-  };
+  const { isVisible, toggleModal } = useModal();
 
   if (!fileData) return null;
 
@@ -70,7 +37,6 @@ function FileItem({
     <div
       className={`grid grid-cols-5 gap-10 p-5 rounded-md text-15 text-center ${fileTypeStyle()}`}
     >
-      {mutation.isPending && <Loader />}
       <div className={'flex items-center col-start-2'}>
         <span>{fileType}</span>
       </div>
@@ -85,41 +51,18 @@ function FileItem({
       <button
         type={'button'}
         className={`text-14 text-center font-normal text-red-500`}
-        onClick={handleOpenUpdateFileInput}
+        onClick={toggleModal}
       >
-        {updateFileInputOpen ? '닫기' : '수정'}
+        파일 수정
       </button>
 
-      {updateFileInputOpen && (
-        <form
-          className={'row-start-2 col-span-4 col-start-2 flex gap-10 text-13'}
-          onSubmit={handleSubmit(onSubmit)}
-        >
-          <div className={'flex flex-col'}>
-            <input
-              {...register('file', fieldRules.file)}
-              id={'file'}
-              type={'file'}
-              className={'w-full bg-white'}
-              accept={'.zip,.rar,.7z,.tar,.gz,.pdf,.hwp,.doc,.docx'}
-              disabled={mutation.isPending}
-            />
-            {errors.file && (
-              <span className={'text-10 text-red-700'}>
-                {errors.file.message}
-              </span>
-            )}
-          </div>
-          <button
-            className={
-              'h-fit px-10 py-2 text-red-500 border-1 rounded-md border-red-300'
-            }
-            disabled={mutation.isPending}
-          >
-            파일 수정
-          </button>
-        </form>
-      )}
+      <FileUpdateModal
+        subLectureId={subLectureId}
+        fileType={fileType}
+        fileId={fileId}
+        isVisible={isVisible}
+        onClose={toggleModal}
+      />
     </div>
   );
 }
---
+++ src/pages/Admin/Class/components/FileUpdateModal.tsx
@@ -0,0 +1,104 @@
+import { X } from '@phosphor-icons/react';
+import { SubmitHandler } from 'react-hook-form';
+
+import Loader from '@/components/Loader';
+import Modal from '@/components/Modal';
+import useFileUpdateForm, {
+  IFileUpdateForm,
+} from '@/pages/Admin/Class/hooks/useFileUpdateForm';
+import useFileUpdateMutation from '@/pages/Admin/Class/hooks/useFileUpdateMutation';
+
+interface FileUpdateModalProps {
+  subLectureId: number;
+  fileType: string;
+  fileId: number;
+  isVisible: boolean;
+  onClose: () => void;
+}
+
+function FileUpdateModal({
+  subLectureId,
+  fileType,
+  fileId,
+  isVisible,
+  onClose,
+}: FileUpdateModalProps) {
+  const { register, errors, handleSubmit, fieldRules, reset } =
+    useFileUpdateForm();
+  const { mutation } = useFileUpdateMutation({ subLectureId, fileId });
+
+  const onSubmit: SubmitHandler<IFileUpdateForm> = data => {
+    const result = window.confirm('파일을 수정하시겠습니까?');
+    if (result) {
+      mutation.mutate(
+        { fileId, file: data.file[0] },
+        {
+          onSuccess: () => {
+            alert('파일이 수정됐습니다.');
+            reset();
+          },
+          onError: error => {
+            alert(error.message || '파일 수정에 실패했습니다.');
+          },
+        },
+      );
+    }
+  };
+
+  return (
+    <Modal size={'sm'} isVisible={isVisible}>
+      {mutation.isPending && <Loader />}
+      <Modal.Header>
+        <button
+          type='button'
+          className='absolute top-12 right-12'
+          onClick={onClose}
+          disabled={mutation.isPending}
+        >
+          <X size={20} />
+        </button>
+      </Modal.Header>
+      <Modal.Content>
+        <form
+          id={'file-update'}
+          className={`w-full pt-10 flex flex-col gap-10 text-16`}
+          onSubmit={handleSubmit(onSubmit)}
+        >
+          <div className={'w-full flex-col-center gap-20'}>
+            <div className={`w-2/3 flex justify-between items-center px-10`}>
+              <span className={'text-neutral-500'}>파일 유형</span>
+              <span className={'p-5 font-medium'}>{fileType}</span>
+            </div>
+            <div className={'w-2/3'}>
+              <input
+                {...register('file', fieldRules.file)}
+                id={'file'}
+                type={'file'}
+                className={'w-full bg-white'}
+                accept={'.zip,.rar,.7z,.tar,.gz,.pdf,.hwp,.doc,.docx'}
+                disabled={mutation.isPending}
+              />
+              {errors.file && (
+                <span className={'text-10 text-red-700'}>
+                  {errors.file.message}
+                </span>
+              )}
+            </div>
+          </div>
+        </form>
+      </Modal.Content>
+      <Modal.Actions>
+        <button
+          type='submit'
+          form={'file-update'}
+          className={'w-1/2 h-40 text-white rounded-md bg-primary-300'}
+          disabled={mutation.isPending}
+        >
+          수정
+        </button>
+      </Modal.Actions>
+    </Modal>
+  );
+}
+
+export default FileUpdateModal;
