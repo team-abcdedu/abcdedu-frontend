f7ed808cde8ed3e449246b36af0d8402ce2a87fc
hyungjpa <100325940+PHJoon@users.noreply.github.com>
2024-11-19 19:22:45 +0900

    refactor: FileUploadModal 로직 수정 #260

--- src/pages/Admin/Class/components/FileUploadModal.tsx
+++ src/pages/Admin/Class/components/FileUploadModal.tsx
@@ -1,7 +1,11 @@
 import { X } from '@phosphor-icons/react';
+import { SubmitHandler } from 'react-hook-form';
 
 import Modal from '@/components/Modal';
-import useFileUpload from '@/pages/Admin/Class/hooks/useFileUpload';
+import useFileUploadForm, {
+  IFileUploadForm,
+} from '@/pages/Admin/Class/hooks/useFileUploadForm';
+import useFileUploadMutation from '@/pages/Admin/Class/hooks/useFileUploadMutation';
 
 interface FileUploadModalProps {
   subClassId: number;
@@ -14,17 +18,39 @@ function FileUploadModal({
   isVisible,
   onClose,
 }: FileUploadModalProps) {
-  const { register, fieldRules, errors, onSubmit } = useFileUpload({
+  const { mutation } = useFileUploadMutation({
     subLectureId: subClassId,
   });
+
+  const { register, errors, handleSubmit, reset, fieldRules } =
+    useFileUploadForm();
+
+  const onSubmit: SubmitHandler<IFileUploadForm> = data => {
+    const result = window.confirm('파일을 업로드합니다.');
+
+    if (result) {
+      mutation.mutate(
+        { type: data.type, file: data.file[0] },
+        {
+          onSuccess: () => {
+            alert('파일이 업로드 되었습니다.');
+            reset();
+          },
+          onError: error => {
+            alert(error.message || '파일 업로드에 실패했습니다.');
+          },
+        },
+      );
+    }
+  };
 
   return (
     <Modal size={'sm'} isVisible={isVisible} onClose={onClose}>
       <Modal.Content>
         <form
           id={'file-upload'}
-          className={'w-full pt-10 flex flex-col gap-10 text-16'}
-          onSubmit={onSubmit}
+          className={`w-full pt-10 flex flex-col gap-10 text-16`}
+          onSubmit={handleSubmit(onSubmit)}
         >
           <button
             type='button'
@@ -39,7 +65,9 @@ function FileUploadModal({
               <span className={'text-neutral-500'}>파일 유형</span>
               <select
                 {...register('type', fieldRules.type)}
+                name={'type'}
                 className={'p-5 font-medium'}
+                disabled={mutation.isPending}
               >
                 <option value={'THEORY'}>이론</option>
                 <option value={'DATA'}>자료</option>
@@ -56,9 +84,11 @@ function FileUploadModal({
               <input
                 {...register('file', fieldRules.file)}
                 id={'file'}
+                name={'file'}
                 type={'file'}
                 className={'w-full border-1'}
                 accept={'.zip,.rar,.7z,.tar,.gz,.pdf,.hwp,.doc,.docx'}
+                disabled={mutation.isPending}
               />
               {errors.file && (
                 <span className={'text-13 text-red-700'}>
@@ -74,6 +104,7 @@ function FileUploadModal({
           type='submit'
           form={'file-upload'}
           className={'w-1/2 h-40 text-white rounded-md bg-primary-300'}
+          disabled={mutation.isPending}
         >
           등록
         </button>
