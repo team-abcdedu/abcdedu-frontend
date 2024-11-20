ed0dfb7d372cd08365fa43f8a02d02eda7933eb1
hyungjpa <100325940+PHJoon@users.noreply.github.com>
2024-11-20 10:43:56 +0900

    refactor: 관리자 테이블 칼럼 데이터 상수 수정 적용 #260

--- src/pages/Admin/constants/index.ts
+++ src/pages/Admin/constants/index.ts
@@ -4,7 +4,7 @@ import { HomeworkSummary } from '@/types/homework';
 import { SurveySummary } from '@/types/survey';
 import { UserSummary } from '@/types/user';
 
-import { AdminTableColumns, TableColumnMap, TableColumns } from '../types';
+import { AdminTableColumns } from '../types';
 
 export const classTableColumns: AdminTableColumns<ClassData> = {
   columnList: ['title', 'subTitle', 'description', 'subClasses'],
@@ -57,58 +57,6 @@ export const userTableColumns: AdminTableColumns<UserSummary> = {
     'createdAt',
   ],
   columnLabels: {
-    memberId: '회원번호',
-    role: '등급',
-    name: '이름',
-    email: '이메일',
-    school: '학교',
-    studentId: '학번',
-    createdAt: '가입일',
-  },
-};
-
-export const tableColumns: TableColumns = {
-  class: ['title', 'subTitle', 'description', 'subClasses'],
-  homework: ['id', 'title', 'description'],
-  survey: ['id', 'createdAt', 'class', 'name'],
-  contact: ['contactId', 'type', 'title', 'userName', 'createdAt'],
-  user: [
-    'memberId',
-    'role',
-    'name',
-    'email',
-    'school',
-    'studentId',
-    'createdAt',
-  ],
-};
-
-export const tableColumnMap: TableColumnMap = {
-  class: {
-    title: '클래스명',
-    subTitle: '부제목',
-    description: '설명',
-    subClasses: '서브클래스',
-  },
-  homework: {
-    id: 'ID',
-    title: '제목',
-    description: '설명',
-  },
-  survey: {
-    id: 'ID',
-    createdAt: '제출일',
-    class: '클래스',
-    name: '이름',
-  },
-  contact: {
-    contactId: 'ID',
-    type: '문의 유형',
-    title: '문의 제목',
-    userName: '작성자',
-    createdAt: '작성일',
-  },
-  user: {
     memberId: '회원번호',
     role: '등급',
     name: '이름',
