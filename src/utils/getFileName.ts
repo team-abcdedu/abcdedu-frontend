395ef071584d417377bdaa0423962a25e16c2174
mihee <s2809h29@gmail.com>
2024-12-11 13:11:37 +0900

    refactor: getFileName 예외 처리 추가
    - 반환 타입 string으로 고정

--- src/utils/getFileName.ts
+++ src/utils/getFileName.ts
@@ -1,5 +1,7 @@
 export const getFileName = (url: string) => {
   const path = url.split('?')[0];
   const fileName = path.split('/').pop();
+
+  if (!fileName) throw new Error('파일 이름을 가져올 수 없습니다.');
   return fileName;
 };
