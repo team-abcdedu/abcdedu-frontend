ed8215c73982c398cb0fc562884ef481a7d80e9c
hyungjpa <100325940+PHJoon@users.noreply.github.com>
2024-12-24 10:50:19 +0900

    feat: useRepliesDownload 추가 #100

---
+++ src/pages/Admin/Homework/hooks/useRepliesDownload.ts
@@ -0,0 +1,46 @@
+import axios from 'axios';
+
+import { IRepliesDownloadForm } from '@/pages/Admin/Homework/hooks/useRepliesDownloadForm';
+import AdminHomeworkApi from '@/services/admin/homework';
+
+interface UseRepliesDownloadProps {
+  homeworkId: number;
+}
+
+function useRepliesDownload({ homeworkId }: UseRepliesDownloadProps) {
+  const handleDownload = async (data: IRepliesDownloadForm) => {
+    try {
+      const response = await AdminHomeworkApi.getHomeworkRepliesExcel({
+        homeworkId,
+        ...data,
+      });
+
+      const fileName = `과제응답(${data.fromDate.slice(2).replaceAll('-', '')}-${data.toDate
+        .slice(2)
+        .replaceAll('-', '')}).xlsx`;
+
+      const link = document.createElement('a');
+      const blob = new Blob([response], {
+        type: 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet',
+      });
+      link.href = URL.createObjectURL(blob);
+      link.download = fileName;
+      link.click();
+
+      URL.revokeObjectURL(link.href);
+      link.remove();
+      return { status: 'success', message: '' };
+    } catch (err) {
+      return {
+        status: 'error',
+        message: axios.isAxiosError(err)
+          ? '파일을 불러오는 중에 문제가 발생했습니다. 잠시 후 다시 시도해주세요.'
+          : '파일 다운로드 중 문제가 발생했습니다. 잠시 후 다시 시도해 주세요.',
+      };
+    }
+  };
+
+  return { handleDownload };
+}
+
+export default useRepliesDownload;
