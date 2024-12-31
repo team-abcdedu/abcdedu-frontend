64cc0c15d4f138e1fdcf122d739c8ef68a688e16
hyungjpa <100325940+PHJoon@users.noreply.github.com>
2025-01-01 00:38:41 +0900

    refactor: HomeworkDescription 다시 추가 #100

--- src/pages/Homework/components/HomeworkLayout.tsx
+++ src/pages/Homework/components/HomeworkLayout.tsx
@@ -1,7 +1,9 @@
 import { useQuery } from '@tanstack/react-query';
 
-import HomeworkForm from '@/pages/Homework/components/HomeworkForm';
 import HomeworkApi from '@/services/homework';
+
+import HomeworkDescription from './HomeworkDescription';
+import HomeworkForm from './HomeworkForm';
 
 function HomeworkLayout() {
   const { data: homework } = useQuery({
@@ -15,23 +17,7 @@ function HomeworkLayout() {
 
   return (
     <div className={'w-full h-full'}>
-      <div
-        className={
-          'w-full min-h-[600px] px-30 flex-col-center gap-40 text-center break-keep'
-        }
-      >
-        <h1 className={'w-full'}>
-          <div className={'text-30 md:text-50 font-bold text-primary-300'}>
-            {homework.title}
-          </div>
-        </h1>
-        <h2 className={`w-full text-18 md:text-22 font-semibold`}>
-          {homework.description}
-        </h2>
-        <p className={`w-full text-16 md:text-20 whitespace-pre-wrap`}>
-          {homework.additionalDescription}
-        </p>
-      </div>
+      <HomeworkDescription homework={homework} />
       <HomeworkForm homework={homework} />
     </div>
   );
