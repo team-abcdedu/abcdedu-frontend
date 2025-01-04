53a68f470d8a6f125c19e958ba9aaac10d966b63
hyungjpa <100325940+PHJoon@users.noreply.github.com>
2025-01-05 01:18:19 +0900

    fix: 과제관리 페이지 개발중인 부분 주석처리 #100

--- src/pages/Admin/Homework/index.tsx
+++ src/pages/Admin/Homework/index.tsx
@@ -1,28 +1,24 @@
-import { useState } from 'react';
-
+// import useModal from '@/hooks/useModal';
+// import CreateHomeworkModal from '@/pages/Admin/Homework/components/CreateHomeworkModal';
 import HomeworkTable from '@/pages/Admin/Homework/components/HomeworkTable';
 
 function Homework() {
-  const [mode, setMode] = useState<'table' | 'register'>('table');
-
-  const handleHomeworkRegister = () => {
-    setMode(mode === 'table' ? 'register' : 'table');
-  };
+  // 과제 생성 개발중
+  // const { isVisible, toggleModal } = useModal();
 
   return (
     <div className={'w-full h-full flex flex-col gap-20'}>
       <div className={'w-full flex justify-between pr-50'}>
-        <h1 className={'text-30 font-semibold'}>
-          {mode === 'table' ? '과제 관리' : '과제 등록'}
-        </h1>
-        <button
-          className={'px-10 text-20 border-2 rounded-lg border-neutral-300'}
-          onClick={handleHomeworkRegister}
-        >
-          {mode === 'table' ? '과제 등록' : '과제 관리'}
-        </button>
+        <h1 className={'text-30 font-semibold'}>과제 관리</h1>
+        {/* <button */}
+        {/*  className={'px-10 text-20 border-2 rounded-lg border-neutral-300'} */}
+        {/*  onClick={toggleModal} */}
+        {/* > */}
+        {/*  과제 생성 */}
+        {/* </button> */}
       </div>
-      {mode === 'table' ? <HomeworkTable /> : null}
+      <HomeworkTable />
+      {/* <CreateHomeworkModal isVisible={isVisible} onClose={toggleModal} /> */}
     </div>
   );
 }
