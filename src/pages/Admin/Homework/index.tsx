389566b7a3940613e343bc6cdc20ea1f2cbabcf5
hyungjpa <100325940+PHJoon@users.noreply.github.com>
2025-01-21 01:32:26 +0900

    feat: 관리자 - 과제관리 페이지 과제 생성 모달 주석 해제 #285

--- src/pages/Admin/Homework/index.tsx
+++ src/pages/Admin/Homework/index.tsx
@@ -1,24 +1,23 @@
-// import useModal from '@/hooks/useModal';
-// import CreateHomeworkModal from '@/pages/Admin/Homework/components/CreateHomeworkModal';
+import useModal from '@/hooks/useModal';
+import CreateHomeworkModal from '@/pages/Admin/Homework/components/CreateHomeworkModal';
 import HomeworkTable from '@/pages/Admin/Homework/components/HomeworkTable';
 
 function Homework() {
-  // 과제 생성 개발중
-  // const { isVisible, toggleModal } = useModal();
+  const { isVisible, toggleModal } = useModal();
 
   return (
     <div className={'w-full h-full flex flex-col gap-20'}>
       <div className={'w-full flex justify-between pr-50'}>
         <h1 className={'text-30 font-semibold'}>과제 관리</h1>
-        {/* <button */}
-        {/*  className={'px-10 text-20 border-2 rounded-lg border-neutral-300'} */}
-        {/*  onClick={toggleModal} */}
-        {/* > */}
-        {/*  과제 생성 */}
-        {/* </button> */}
+        <button
+          className={'px-10 text-20 border-2 rounded border-black'}
+          onClick={toggleModal}
+        >
+          과제 생성
+        </button>
       </div>
       <HomeworkTable />
-      {/* <CreateHomeworkModal isVisible={isVisible} onClose={toggleModal} /> */}
+      <CreateHomeworkModal isVisible={isVisible} onClose={toggleModal} />
     </div>
   );
 }
